1e06ca8dd0c1fe68f37b657b6a137fc37b2049f4
gabrielbessler <gabrielbessler@gmail.com>
2019-12-27 11:17:37 -0800

    Python Test File

--- src/tests/test_python.py
+++ src/tests/test_python.py
@@ -1,10 +1,43 @@
-# Expr 
-# Return 
-# Assign 
-# For 
-# With 
-# If 
-# Raise 
-# Try 
-# While 
-# PythonConvert::toGraph
+import unittest 
+
+class TestPythonConvert(unittest.TestCase): 
+    # Expr 
+    def testExpr(self): 
+        pass
+
+    # Return 
+    def testReturn(self): 
+        pass 
+
+    # Assign 
+    def testAssign(self): 
+        pass 
+
+    # For 
+    def testFor(self): 
+        pass 
+
+    # With 
+    def testWith(self): 
+        pass 
+
+    # If 
+    def testIf(self): 
+        pass 
+
+    # Raise 
+    def testRaise(self): 
+        pass 
+
+    # Try 
+    def testTry(self): 
+        pass 
+
+    # While 
+    def testWhile(self): 
+        pass 
+
+    # PythonConvert::toGraph
+    def testToGraph(self): 
+        pass 
+        
