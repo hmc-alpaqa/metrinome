6dd0c501b9279ec3164f12f3521f3367f69aeb94
ishaangandhi <ishaangandhi@gmail.com>
2020-01-19 11:13:17 -0500

    Added graph tests
    You must run
    
    ```pip install -e .```
    
    before running tests in the directory that has setup.py in order for the system to discover the packages. Otherwise Graph is unavailable.

--- src/tests/test_graph.py
+++ src/tests/test_graph.py
@@ -1,5 +1,6 @@
 import unittest
-# from Graph import Graph
+from Graph import Graph
+import numpy as np
 
 class TestGraph(unittest.TestCase):
     # Graph(edges, vertices, startNode, endNode)
@@ -7,43 +8,53 @@ class TestGraph(unittest.TestCase):
     # === Graph::edgeRules ===
     def testEdgeRules_NoEdges(self):
         g = Graph([], [1, 2, 3], 1, 3)
-        g.edgeRules()
+        edge_list = g.edgeRules()
+        self.assertEqual(edge_list, [])
 
     # === Graph::vertexCount ===
     def testVertexCount_OneVertex(self):
         g = Graph([], [1], 1, 1)
-        g.vertexCount()
+        vertex_count = g.vertexCount()
+        self.assertEqual(vertex_count, 1)
 
     def testVertexCount_ManyVertices(self): 
         g = Graph([[1, 2], [2, 3], [2, 4]], [1, 2, 3, 4], 1, 4)
-        g.vertexCount()
+        vertex_count = g.vertexCount()
+        self.assertEqual(vertex_count, 4)
 
     # === Graph::getVertices ===
     def testGetVertices_OneVertex(self):
         g = Graph([], [1], 1, 1)
-        g.getVertices()
+        vertices = g.getVertices()
+        self.assertEqual(vertices, [1])
 
     def testVertices_ManyVertices(self): 
         g = Graph([[1, 2], [2, 3], [2, 4]], [1, 2, 3, 4], 1, 4)
-        g.getVertices()
+        vertices = g.getVertices()
+        self.assertEqual(vertices, [1, 2, 3, 4])
 
     # === Graph::advacencyMatrix ===
     def testAdjacencyMatrix_NoEdges(self):
         g = Graph([], [1, 2, 3], 1, 3)
-        g.adjacencyMatrix()
+        adj_mat = g.adjacencyMatrix()
+        empty_arr = np.zeros((4, 4))
+        self.assertTrue((adj_mat == empty_arr).all())
         
     def testAdjacencyList_NormalGraph(self):
         g = Graph([[1, 2], [2, 3], [2, 4]], [1, 2, 3, 4], 1, 4)
-        g.adjacencyMatrix()
+        adj_mat = g.adjacencyMatrix()
+        expected_mat = [[0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 1, 0],
+                        [0, 1, 0, 0, 1],
+                        [0, 0, 0, 0, 0]]
+        self.assertTrue((adj_mat == expected_mat).all())
 
     # === Graph::adjacencyList ===
     def testAdjacencyList_NoEdges(self):
         g = Graph([], [1, 2, 3], 1, 3)
-        g.adjacencyList()
-
-    def testAdjacencyList_NormalGraph(self): 
-        g = Graph([[1, 2], [2, 3], [2, 4]], [1, 2, 3, 4], 1, 4)
-        g.adjacencyList()
+        adj_list = g.adjacencyList()
+        self.assertEqual(adj_list, [[], [], []])
 
     # === Graph::fromFile ===
     def testFromFile_OneVertex(self):
