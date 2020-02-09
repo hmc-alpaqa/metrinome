3a0c75d42e856b1fe1f1a856773d3351f3820693
Gabriel Bessler <gabrielbessler@Gabriels-MacBook-Pro.local>
2020-02-09 13:55:22 -0800

    Added tests for Log.py, some testing function to test utils

--- src/tests/test_log.py
+++ src/tests/test_log.py
@@ -1,17 +1,38 @@
 import unittest
+import sys 
+from utils import captured_output
+sys.path.append("/app/code/")
+from Log import Log
 
 class TestLog(unittest.TestCase):
     def testSetTag(self): 
-        pass 
+        l = Log(TAG="test_tag", display_output=True)
+        with captured_output() as (out, err):
+            l.setTag("test_tag_2")
+            l.v("{} ~ {}")
+        expected_msg = "test_tag_2 > {} ~ {}"
+        self.assertEqual(expected_msg, out.getvalue().rstrip())
 
     def test_i(self):
-        pass 
+        l = Log(TAG="test_tag", display_output=True)
+        with captured_output() as (out, err):
+            l.i("test_msg") 
+        expected_msg = "test_tag ~~~ TEST_MSG ~~~"
+        self.assertEqual(expected_msg, out.getvalue().rstrip())
 
     def test_v(self):
-        pass 
+        l = Log(TAG="test_tag", display_output=True)
+        with captured_output() as (out, err):
+            l.v("test_msg")  
+        expected_msg = "test_tag > test_msg"
+        self.assertEqual(expected_msg, out.getvalue().rstrip())
 
     def test_e(self): 
-        pass 
+        l = Log(TAG="test_tag", display_output=True)
+        with captured_output() as (out, err):
+            l.e("test_msg") 
+        expected_msg = "test_tag > !!! test_msg !!!"
+        self.assertEqual(expected_msg, out.getvalue().rstrip())
 
 if __name__ == '__main__':
     unittest.main()
