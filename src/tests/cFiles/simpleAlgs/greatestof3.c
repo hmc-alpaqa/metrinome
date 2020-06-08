5e95b01ab86715f4f6678c661c7ca29e1815eac1
Shathebean <shaheen@gmail.com>
2020-06-08 16:19:14 -0700

    Formatted first page of algorithms.

--- src/tests/cFiles/simpleAlgs/greatestof3.c
+++ src/tests/cFiles/simpleAlgs/greatestof3.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
+
+
+void greatestof3(int num1, int num2, int num3){
+
+  if((num1>num2)&&(num1>num3))
+     printf("\n Number1 is greatest");
+  else if((num2>num3)&&(num2>num1))
+     printf("\n Number2 is greatest");
+  else
+     printf("\n Number3 is greatest");
+}
 int main()
 {
-   int num1,num2,num3;
-
-   //Ask user to input any three integer numbers
-   printf("\nEnter value of num1, num2 and num3:");
-   //Store input values in variables for comparsion
-   scanf("%d %d %d",&num1,&num2,&num3);
-
-   if((num1>num2)&&(num1>num3))
-      printf("\n Number1 is greatest");
-   else if((num2>num3)&&(num2>num1))
-      printf("\n Number2 is greatest");
-   else
-      printf("\n Number3 is greatest");
-   return 0;
+  greatestof3(5,12,2);
 }
--- src/tests/cFiles/simpleAlgs/selectionsort.c
+++ src/tests/cFiles/simpleAlgs/selectionsort.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
+void selectionsort(int count, int number[]){
+  int i, j, temp;
+  // Logic of selection sort algorithm
+  for(i=0;i<count;i++){
+     for(j=i+1;j<count;j++){
+        if(number[i]>number[j]){
+           temp=number[i];
+           number[i]=number[j];
+           number[j]=temp;
+        }
+     }
+  }
+
+  printf("Sorted elements: ");
+  for(i=0;i<count;i++)
+     printf(" %d",number[i]);
+}
 int main(){
    /* Here i & j for loop counters, temp for swapping,
     * count for total number of elements, number[] to
     * store the input numbers in array. You can increase
     * or decrease the size of number array as per requirement
     */
-   int i, j, count, temp, number[25];
+   int i, count, number[25];
 
    printf("How many numbers u are going to enter?: ");
    scanf("%d",&count);
@@ -15,20 +32,7 @@ int main(){
    for(i=0;i<count;i++)
       scanf("%d",&number[i]);
 
-   // Logic of selection sort algorithm
-   for(i=0;i<count;i++){
-      for(j=i+1;j<count;j++){
-         if(number[i]>number[j]){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
-      }
-   }
 
-   printf("Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
-
-   return 0;
+  selectionsort(count, number);
+  return 0;
 }
