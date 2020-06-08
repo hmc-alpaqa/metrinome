ca01d46fbd65d1de07ae415f318bfbfe90e86c01
Lucas Bang <bang@system76-pc.localdomain>
2020-06-08 15:31:36 -0700

    add test code to find value in an unsorted array

--- src/tests/cFiles/simpleAlgs/find_val_in_array.c
+++ src/tests/cFiles/simpleAlgs/find_val_in_array.c
@@ -12,7 +12,12 @@ int find_first(int val, int* array, int size){
 
 int main(){
     const int n = 10;
+    int i;
     int a[n] = {1,2,3,4,5,4,3,2,1,0};
-    int i = find_first(3, a, n);
+     
+    i = find_first(3, a, n);
+    printf("%d\n", i);
+
+    i = find_first(19, a, n);
     printf("%d\n", i);
 }
