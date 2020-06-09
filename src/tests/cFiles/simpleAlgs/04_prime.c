7735bbabd1b2287f7a08d6c64b2fc17a56f8a696
Lucas Bang <bang@cs.hmc.edu>
2020-06-08 19:15:06 -0700

    refactor rename primt.

---
+++ src/tests/cFiles/simpleAlgs/04_prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int check_prime(int n)
+{
+   int c;
+   for ( c = 2 ; c <= n - 1 ; c++ )
+   { 
+     if ( n % c == 0 )
+     return 0;
+   }
+   return 1;
+} 
+ 
+int main()
+{
+   int n, result;
+ 
+   n = 23;
+
+   result = check_prime(n);
+ 
+   printf("%d\n", result);   
+   return 0;
+}
+ 
