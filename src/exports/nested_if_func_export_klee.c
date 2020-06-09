8cf69b96fd994b8d177f2d937afb29a85b493365
Gabriel <gabrielbessler@gmail.com>
2020-06-08 23:13:21 -0500

    Remove unused files

--- src/exports/nested_if_func_export_klee.c
+++
@@ -1,9 +0,0 @@
-#include <klee/klee.h>
-#include </app/code/tests/cFiles/nested_if.c>
-#define SIZE 10
-int main() {
-
-	int x;
-	klee_make_symbolic(&x, sizeof(x), "518e33d2f14e421c8274815b26a850a6");
-	return nested_if_func(x);
-}
