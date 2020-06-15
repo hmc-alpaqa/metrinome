d1dfe927bf96365a0adfcb75db3f3392f553b632
Lucas Bang <bang@cs.hmc.edu>
2020-06-15 14:03:53 -0700

    move unused files. refactor LCS. refactor LCIS.

--- src/tests/cFiles/simpleAlgs/18_longest_common_subsequence_main.c
+++ src/tests/cFiles/simpleAlgs/18_longest_common_subsequence_main.c
@@ -1,12 +1,27 @@
 #include "18_longest_common_subsequence.c"
 
+// The longest common subsequence in C
+
 int main() {
-  int arr1[] = {3, 4, 9, 1};
-  int arr2[] = {5, 3, 8, 9, 10, 2, 1};
+  int S1[MAXSIZE] = {1,3,1,4,2}; //"ACADB"}; 
+  int S2[MAXSIZE] = {3,2,4,1,2}; //"CBDA";
+  int m = 5;
+  int n = 5;
 
-  int n = sizeof(arr1) / sizeof(arr1[0]);
-  int m = sizeof(arr2) / sizeof(arr2[0]);
+  // for(int i = 0; i < m; i++){
+  // 	printf("%d ", S1[i]);
+  // }
+  // printf("\n");
 
-  printf("Length of LCIS is %d\n", LCIS(arr1, n, arr2, m));
-  return (0);
+  // for(int i = 0; i < n; i++){
+  // 	printf("%d ", S2[i]);
+  // }
+  // printf("\n");
+
+  int lcs_length = lcs(S1, m, S2, n);
+  printf("LCS length = %d\n", lcs_length);
 }
+
+
+
+
