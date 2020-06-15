204569857d01c79ea5f3cf33d9ac5a66a82e79a1
Lucas Bang <bang@cs.hmc.edu>
2020-06-15 09:37:35 -0700

    qs main.

--- src/tests/cFiles/simpleAlgs/26_quicksort.c
+++ src/tests/cFiles/simpleAlgs/26_quicksort.c
@@ -9,28 +9,6 @@ void swap(int* a, int* b)
     *b = t; 
 } 
   
-/* This function is same in both iterative and recursive*/
-// int partition(int arr[], int l, int h) 
-// { 
-//     int x = arr[h]; 
-//     int i = (l - 1); 
-  
-//     for (int j = l; j <= h - 1; j++) { 
-//         if (arr[j] <= x) { 
-//             i++;
-//             // int temp = arr[i];
-//             // arr[i] = arr[j];
-//             // arr[j] = temp; 
-//             swap(&arr[i], &arr[j]); 
-//         } 
-//     } 
-//     swap(&arr[i + 1], &arr[h]); 
-//     return (i + 1); 
-// } 
-  
-/* A[] --> Array to be sorted,  
-   l  --> Starting index,  
-   h  --> Ending index */
 void quickSortIterative(int arr[], int l, int h) 
 { 
     // Create an auxiliary stack 
@@ -84,20 +62,3 @@ void quickSortIterative(int arr[], int l, int h)
     } 
 } 
   
-// A utility function to print contents of arr 
-void printArr(int arr[], int n) 
-{ 
-    int i; 
-    for (i = 0; i < n; ++i) 
-        printf("%d ", arr[i]); 
-} 
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
-    int n = sizeof(arr) / sizeof(*arr); 
-    quickSortIterative(arr, 0, n - 1); 
-    printArr(arr, n); 
-    return 0; 
-} 
