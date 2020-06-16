544309cbaa719e66e87d09f21248574522d1874a
Lucas Bang <bang@cs.hmc.edu>
2020-06-16 12:47:36 -0700

    sorted or reverse quadratic??

---
+++ src/tests/cFiles/simpleAlgs/50_check_sorted_or_reverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int is_sorted_or_reverse(int array[], int size) {
+  int not_sorted = 0;
+  for (int i = 0; i < size - 1; ++i) {
+    if (array[i] > array[i + 1]) {
+      not_sorted = 1;
+      break;
+    }
+  }
+
+  int not_reversed = 0;
+  for (int i = 0; i < size - 1; ++i) {
+    if (array[i] < array[i + 1]) {
+      not_reversed = 1;
+      break;
+    }
+  }
+
+  return !not_sorted || !not_reversed;
+}
---
+++ src/tests/cFiles/simpleAlgs/50_check_sorted_or_reverse_main.c
@@ -0,0 +1,23 @@
+#include "50_check_sorted_or_reverse.c"
+
+int main() {
+  const int n = 10;
+  int s;
+  int a[n] = {1, 2, 4, 4, 5, 7, 10, 10, 19, 23};
+
+  s = is_sorted_or_reverse(a, n);
+  printf("%d\n", s);
+
+
+  int b[n] = {1, 2, 4, 4, 5, 11, 10, 10, 19, 23};
+
+  s = is_sorted_or_reverse(b, n);
+  printf("%d\n", s);
+
+  int c[n] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10};
+
+  s = is_sorted_or_reverse(c, n);
+  printf("%d\n", s);
+
+
+}
