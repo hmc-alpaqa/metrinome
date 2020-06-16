500cd60186476b9fe0a9bae831b5b4a250abef1a
Gabriel <gabrielbessler@gmail.com>
2020-06-16 15:56:57 -0500

    Auto-Breakpointer now picks between exponential and linear

--- src/analyze_new.py
+++ src/analyze_new.py
@@ -111,32 +111,10 @@ def generate_graphs(func, field):
 from scipy import optimize
 import matplotlib.pyplot as plt
 import numpy as np
+from functools import partial
+from time import time 
 
-if __name__ == "__main__":
-    from time import time 
-    st = time()
-    f = "22_selectionsort"
-    data = pd.read_csv(f"/Users/gabrielbessler/repos/path-complexity/{f}.csv")
-    times = [float(i.split()[2]) for i in data.iloc[:,0]]
-    data_field = data["CompletedPaths"]
-    # X = np.array([1, 1.5, 2, 2.4, 3, 3.6, 4, 5, 6, 7, 8, 9, 10 ,11, 12, 13, 14, 15, 16, 17], dtype=float)
-    # Y = np.array([5, 6.1, 7, 8.35, 9, 10, 11, 13, 15, 28.92, 42.81, 56.7, 70.59, 84.47, 98.36, 112.25, 126.14, 140.03, 152, 163])
-    X = np.array(times)
-    Y = np.array(data_field)
-
-    # Suppose we always have exactly one breakpoint.
-    degree_one = None
-    degree_two = 1
-    # initial_breakpoint = 2.5
-    # Initialize all of the parameters.
-    # params = [initial_breakpoint] + \
-    num_params = degree_one if degree_one is not None else 3
-    num_params2 = degree_two if degree_two is not None else 3
-    params = [1 for i in range(num_params + 1)] + \
-             [1 for j in range(num_params2 + 1)]
-    # Define the polynomial function based on the degrees.
-    from functools import partial
-    def piecewise_poly(x, params, degree_one, degree_two, bp):
+def piecewise_poly(x, params, degree_one, degree_two, bp):
         num_params = degree_one if degree_one is not None else 3
         num_params2 = degree_two if degree_two is not None else 3
         if degree_one is not None:
@@ -150,6 +128,14 @@ if __name__ == "__main__":
             fn2 = lambda x: params[num_params + 1] + params[num_params + 2] * (params[num_params + 3]**x)
         return np.piecewise(x, [x < bp, x >= bp], [fn1, fn2])
 
+def fit(X, Y, degree_one, degree_two):
+    st = time()
+    # Initialize all of the parameters.
+    # params = [initial_breakpoint] + \
+    num_params = degree_one if degree_one is not None else 3
+    num_params2 = degree_two if degree_two is not None else 3
+    params = [1 for i in range(num_params + 1)] + \
+             [1 for j in range(num_params2 + 1)]
     # Hyperparameter selection - breakpoint
     min_loss = float('inf')
     best_bp = -1
@@ -170,19 +156,62 @@ if __name__ == "__main__":
     # Create a function that takes as make arguments as we have parameters, as well as the 
     # independent variable and the breakpoint value.
     func_to_optimize = lambda x, *params: fit_func(x, params)
+    p, e = optimize.curve_fit(func_to_optimize, X, Y, p0=params, maxfev=5000)
+    l2_loss = sum((Y - func_to_optimize(X, *p))**2)
+    return l2_loss, best_bp
+ 
+if __name__ == "__main__":
+    f = "04_prime"
+    data = pd.read_csv(f"/Users/gabrielbessler/repos/path-complexity/src/{f}.csv")
+    times = [float(i.split()[2]) for i in data.iloc[:,0]]
+    data_field = data["CompletedPaths"]
+    X = np.array(times)
+    Y = np.array(data_field)
+
+    try:
+        err1, best_bp1 = fit(X, Y, 1, 1)
+    except:
+        err1, best_bp1 = float('inf'), None
+    try:
+        err2, best_bp2 = fit(X, Y, None, 1)
+    except:
+        err2, best_bp2 = float('inf'), None
+    try:
+        err3, best_bp3 = fit(X, Y, 1, None)
+    except:
+        err3, best_bp3 = float('inf'), None
+    
+    min_val = min(err1, err2, err3)
+    if min_val == err1:
+        degree_one = 1
+        degree_two = 1
+        best_bp = best_bp1
+    elif min_val == err2:
+        degree_one = None
+        degree_two = 1
+        best_bp = best_bp2
+    elif min_val == err3:
+        degree_one = 1
+        degree_two = None
+        best_bp = best_bp3
+
+    fit_func = partial(piecewise_poly, degree_one=degree_one, degree_two=degree_two, bp=best_bp)
+    # Create a function that takes as make arguments as we have parameters, as well as the 
+    # independent variable and the breakpoint value.
+    num_params = degree_one if degree_one is not None else 3
+    num_params2 = degree_two if degree_two is not None else 3
+    params = [1 for i in range(num_params + 1)] + \
+             [1 for j in range(num_params2 + 1)]
+    func_to_optimize = lambda x, *params: fit_func(x, params)
     for i in range(len(X)):
         if X[i] > best_bp:
             xd = np.linspace(0, X[i - 2], 100)
             xd2 = np.linspace(X[i-1], X[-1], 100)
             break
     p, e = optimize.curve_fit(func_to_optimize, X, Y, p0=params, maxfev=5000)
-    # xd = np.linspace(0, X[-1], 100)
     plt.plot(X, Y, "o")
     plt.plot(xd, func_to_optimize(xd, *p), "r")
     plt.plot(xd2, func_to_optimize(xd2, *p), "b")
     plt.legend(["it's a graph", f"Line 1: {p[0:num_params+1]}", f"Line 2: {p[num_params+1:]}"])
+    plt.show()
     
-    # plt.show()
-    print(f"Best BP Is: {best_bp}")
-    print(f"took {time() - st}")
-    # print(SegmentedLinearReg2(X, Y, bp, 2))
