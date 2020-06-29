1fc84e4ddc8b0eff7d4eb7ac3f0045ab7b80d07f
emily <emilylu232@gmail.com>
2020-06-29 14:30:19 -0700

    added in-lining test for nested while loop

---
+++ src/tests/cFiles/inlining_tests/test-06.c
@@ -0,0 +1,18 @@
+# include <stdio.h> 
+
+int main(){
+    int a;
+    int b;
+    int c;
+    int d;
+
+    while (a < 10) {
+        while (b < 10) {
+            ++c;
+        }
+    }
+    d = a+b;
+
+    return 0;
+}
+ 
