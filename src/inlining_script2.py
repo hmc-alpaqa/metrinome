dcbd25a7bd817056818352b03c5d1f92c4010e60
emily <emilylu232@gmail.com>
2020-07-13 11:36:50 -0700

    added a 2nd in-lining script w heuristic approach

---
+++ src/inlining_script2.py
@@ -0,0 +1,46 @@
+"""Adds always inline attribute to C Files."""
+
+def in_lining(file):
+    """Create inlined version of existing C file."""
+    # open the file that needs to be inlined
+    with open(file, 'r') as old_f:
+        # open new file to write in-lined version into
+        with open((file.split('.')[0]) + "-auto-inline.c", "w") as new_f:
+            # write the new file
+            for line in old_f:
+
+                line_inline = False 
+                if ((line.startswith('int')) or (line.startswith('void')) or (line.startswith('double')) or (line.startswith('bool'))
+                or (line.startswith('float')) or (line.startswith('char')) or (line.startswith('static'))):
+                    if ('(' in line) and (')' in line):
+                        if ('main' not in line) and ('=' not in line):
+                            line_inline = True
+
+                if (line_inline):
+                    new_f.write("__attribute__((always_inline)) inline " + line)
+                else:
+                    new_f.write(line)
+
+def main():
+    """Create inline versions of all test files."""
+    # files_to_inline = ['test-01-un-inlined.c', 'test-02-un-inlined.c', 'test-03-un-inlined.c',
+    #                    'test-04-un-inlined.c', 'test-05-un-inlined.c', 'test-06-un-inlined.c',
+    #                    'test-07-un-inlined.c', 'test-08-un-inlined.c', 'test-09-un-inlined.c',
+    #                    'test-10-un-inlined.c', 'test-11-un-inlined.c', 'test-12-un-inlined.c']
+    
+    # files_to_inline = ['test-20-un-inlined.c', 'test-21-un-inlined.c', 'test-22-un-inlined.c',
+    #                    'test-23-un-inlined.c', 'test-25-un-inlined.c']
+
+    file = "chgrp-un-inlined.c"
+    #in_lining(f"/app/code/tests/cFiles/coreutils-8.32/src/{file}")
+    in_lining(f"/app/code/tests/cFiles/inlining_tests/{file}")
+
+    # for file in files_to_inline:
+    #     print(file)
+    #     in_lining(f"/app/code/tests/cFiles/coreutils-8.32/src/{file}")
+    #     #in_lining(f"/app/code/tests/cFiles/inlining_tests/{file}")
+    #     #in_lining('/app/code/tests/cFiles/inlining_tests/'+str(file))
+
+
+if __name__ == "__main__":
+    main()
