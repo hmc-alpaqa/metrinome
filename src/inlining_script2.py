d72b20160e0935f4c877953ae40fc30f60f1c535
Gabriel <gabrielbessler@gmail.com>
2020-07-13 17:39:11 -0500

    small improvements

--- src/inlining_script2.py
+++ src/inlining_script2.py
@@ -1,5 +1,6 @@
 """Adds always inline attribute to C Files."""
 
+
 def in_lining(file):
     """Create inlined version of existing C file."""
     # open the file that needs to be inlined
@@ -8,18 +9,14 @@ def in_lining(file):
         with open((file.split('.')[0]) + "-auto-inline.c", "w") as new_f:
             # write the new file
             for line in old_f:
-
-                line_inline = False 
-                if ((line.startswith('int')) or (line.startswith('void')) or (line.startswith('double')) or (line.startswith('bool'))
-                or (line.startswith('float')) or (line.startswith('char')) or (line.startswith('static'))):
-                    if ('(' in line) and (')' in line):
-                        if ('main' not in line) and ('=' not in line):
-                            line_inline = True
-
-                if (line_inline):
+                prefixes = ('int', 'void', 'double', 'bool', 'float', 'char', 'static')
+                if any(line.startswith(prefix) for prefix in prefixes) and \
+                   ('(' in line) and (')' in line) and \
+                   ('main' not in line) and ('=' not in line):
                     new_f.write("__attribute__((always_inline)) inline " + line)
                 else:
                     new_f.write(line)
+
 
 def main():
     """Create inline versions of all test files."""
@@ -27,12 +24,12 @@ def main():
     #                    'test-04-un-inlined.c', 'test-05-un-inlined.c', 'test-06-un-inlined.c',
     #                    'test-07-un-inlined.c', 'test-08-un-inlined.c', 'test-09-un-inlined.c',
     #                    'test-10-un-inlined.c', 'test-11-un-inlined.c', 'test-12-un-inlined.c']
-    
+
     # files_to_inline = ['test-20-un-inlined.c', 'test-21-un-inlined.c', 'test-22-un-inlined.c',
     #                    'test-23-un-inlined.c', 'test-25-un-inlined.c']
 
     file = "chgrp-un-inlined.c"
-    #in_lining(f"/app/code/tests/cFiles/coreutils-8.32/src/{file}")
+    # in_lining(f"/app/code/tests/cFiles/coreutils-8.32/src/{file}")
     in_lining(f"/app/code/tests/cFiles/inlining_tests/{file}")
 
     # for file in files_to_inline:
