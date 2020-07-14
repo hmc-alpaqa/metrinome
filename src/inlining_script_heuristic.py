23ea77f94bb5eff43ed3689f9c6ae7dff284345e
emily <emilylu232@gmail.com>
2020-07-14 16:11:23 -0700

    script checks whether 'recursion' is found

--- src/inlining_script_heuristic.py
+++ src/inlining_script_heuristic.py
@@ -8,6 +8,7 @@ def in_lining(file):
     """Create inlined version of existing C file."""
     # open the file that needs to be inlined
     num = 0
+    has_recursion = False
     with open(INPUT_PATH + file, 'r') as old_f:
         # open new file to write in-lined version into
         with open(DESTINATION_PATH + file.split('.')[0] + "-auto-inline.c", "w") as new_f:
@@ -25,7 +26,11 @@ def in_lining(file):
                     num += 1
                 else:
                     new_f.write(line)
-            print(num)
+
+                if ('recurse' in line) or ('recursion' in line) or ('recursive' in line):
+                    has_recursion = True
+                    
+            print(file, num, has_recursion)
 
 
 
@@ -40,14 +45,15 @@ def main():
     # files_to_inline = ['test-20-un-inlined.c', 'test-21-un-inlined.c', 'test-22-un-inlined.c',
     #                    'test-23-un-inlined.c', 'test-25-un-inlined.c']
 
-    # files_to_inline = ['basename.c', 'basenc.c', 'cat.c', 'chcon.c', 'chgrp.c', 'chmod.c',
+    # files_to_inline = ['basenc.c', 'cat.c', 'chcon.c', 'chgrp.c', 'chmod.c',
     #                    'chown-core.c', 'chown.c']
 
     for file in os.listdir(INPUT_PATH):
         if file.endswith(".c"):
-            print(file)
             in_lining(file)
-        # in_lining(f"/app/code/tests/cFiles/inlining_tests/{file}")
+
+    # for file in files_to_inline:
+    #     in_lining(file)
 
 
 if __name__ == "__main__":
