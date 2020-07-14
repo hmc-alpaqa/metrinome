a3925310ae807dc4916ec8205e591cb0331aa624
Ibrahim <iabughararh@g.hmc.edu>
2020-07-14 22:11:45 +0300

    changed the heuristic script to handle coreutils

--- src/inlining_script_heuristic.py
+++ src/inlining_script_heuristic.py
@@ -1,26 +1,27 @@
 """Adds always inline attribute to C Files."""
-
+INPUT_PATH="/app/code/tests/cFiles/coreutils-8.32/src/"
+DESTINATION_PATH="/app/code/tests/cFiles/coreutils-inlined-heuristic/"
 
 def in_lining(file):
     """Create inlined version of existing C file."""
     # open the file that needs to be inlined
     num = 0
-    with open(file, 'r') as old_f:
+    with open(INPUT_PATH+file, 'r') as old_f:
         # open new file to write in-lined version into
-        with open((file.split('.')[0]) + "-auto-inline.c", "w") as new_f:
+        with open(DESTINATION_PATH+file.split('.')[0] + "-auto-inline.c", "w") as new_f:
             # write the new file
             for line in old_f:
                 prefixes = ('int', 'void', 'double', 'bool', 'float', 'char', 'static', 'extern')
                 if ('inline' in line):
-                   new_f.write("__attribute__((always_inline)) " + line)
-                   num += 1
-                   print(num)
+                    new_f.write("__attribute__((always_inline)) " + line)
+                    num += 1
+                    print(num)
                 elif any(line.startswith(prefix) for prefix in prefixes) and \
                    (('(' in line) and (('{' not in line) and (')' not in line) or (('{' in line) and (')' in line))))and \
                    ('main' not in line) and ('=' not in line):
-                   new_f.write("__attribute__((always_inline)) inline " + line)
-                   num += 1
-                   print(num)
+                    new_f.write("__attribute__((always_inline)) inline " + line)
+                    num += 1
+                    print(num)
                 else:
                     new_f.write(line)
 
@@ -35,12 +36,11 @@ def main():
     # files_to_inline = ['test-20-un-inlined.c', 'test-21-un-inlined.c', 'test-22-un-inlined.c',
     #                    'test-23-un-inlined.c', 'test-25-un-inlined.c']
 
-    files_to_inline = ['chroot.c']
-    #files_to_inline = ['basename.c', 'basenc.c', 'cat.c', 'chcon.c', 'chgrp.c', 'chmod.c', 'chown-core.c', 'chown.c']
+    files_to_inline = ['basename.c', 'basenc.c', 'cat.c', 'chcon.c', 'chgrp.c', 'chmod.c', 'chown-core.c', 'chown.c']
 
     for file in files_to_inline:
         print(file)
-        in_lining(f"/app/code/tests/cFiles/coreutils-8.32/src/{file}")
+        in_lining(file)
         #in_lining(f"/app/code/tests/cFiles/inlining_tests/{file}")
 \
 
