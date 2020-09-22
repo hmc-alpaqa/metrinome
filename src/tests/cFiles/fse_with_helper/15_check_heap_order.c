5291e1517b68ab790a4881612d66dad95c91a8d0
emily <emilylu232@gmail.com>
2020-09-22 15:59:19 -0700

    renamed folder and added another inlining test

---
+++ src/tests/cFiles/fse_with_helper/15_check_heap_order.c
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Iterative function to check if given array represents Min-Heap or not
+bool checkMinHeap(int A[], int n) {
+	// check for all internal nodes that their left child and
+	// right child (if present) holds min-heap property or not
+	for (int i = 0; i <= (n - 2) / 2; i++) {
+		if (A[i] > A[2 * i + 1] || ((2 * i + 2 != n) && A[i] > A[2 * i + 2])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int A[] = { 2, 3, 5, 8, 10 };
+	int n = sizeof(A) / sizeof(int);
+
+	if (checkMinHeap(A, n))
+		cout << "Given array is a min heap";
+	else
+		cout << "Given array is not a min heap";
+
+	return 0;
+}
