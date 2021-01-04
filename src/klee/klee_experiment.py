5372fe8b9b05bb4388452852fb694cbe008631e7
Gabriel Bessler <gabrielbessler@gmail.com>
2021-01-03 22:48:42 -0600

    Combined klee experiments.

--- src/klee/klee_experiment.py
+++ src/klee/klee_experiment.py
@@ -1,6 +1,7 @@
 """Script for running Klee on a series of functions and saving data."""
 
 import subprocess
+import sys
 from typing import List
 
 from core.log import Log
@@ -8,9 +9,28 @@ from klee.klee_utils import (KleeUtils, create_pandas, graph_stat,
                              klee_command, klee_compare, run_experiment)
 
 
+def klee_experiment(func: str, array_size: int, max_depths: List[str],
+                    preferences: List[str], fields: List[str], inputs: List[str]) -> None:
+    """Run the KLEE experiment for a single function."""
+    filename = f"/app/code/tests/cFiles/fse_2020_benchmark/{func}.c"
+    output = KleeUtils(Log()).show_func_defs(filename, size=array_size)
+
+    for i in output:
+        new_name = f"/app/code/tests/cFiles/fse_2020_benchmark/{func}_{i}.c"
+        bcname = f"/app/code/tests/cFiles/fse_2020_benchmark/{func}_{i}.bc"
+        with open(new_name, "w+") as file:
+            file.write(output[i])
+        subprocess.run(klee_command(bcname, new_name), shell=True, capture_output=True, check=True)
+        results = klee_compare(bcname, preferences, max_depths, inputs, f"{func}_{i}")
+        results_frame = create_pandas(results, preferences[0], inputs[0], max_depths, fields)
+        results_frame.to_csv(f'/app/code/tests/cFiles/fse_2020_benchmark/frames/{func}.csv')
+        for field in fields:
+            graph_stat(func, preferences[0], max_depths, inputs, results, None, field)
+
+
 # pylint: disable=too-many-locals
-def run_klee_experiment(func: str, array_size: int, max_depths: List[str],
-                        preferences: List[str], fields: List[str], inputs: List[str]) -> None:
+def klee_optimized_experiment(func: str, array_size: int, max_depths: List[str],
+                              preferences: List[str], fields: List[str], inputs: List[str]) -> None:
     """Run the KLEE experiment for a single function."""
     filename = f"/app/code/tests/cFiles/fse_2020_benchmark/{func}.c"
     output = KleeUtils(Log()).show_func_defs(filename, size=array_size)
@@ -42,8 +62,12 @@ def run_klee_experiment(func: str, array_size: int, max_depths: List[str],
 
 def main() -> None:
     """Run the klee experiment."""
-    run_experiment("--dump-states-on-halt=false --max-time=5min", ['1', '2', '3', '4'],
-                   run_klee_experiment)
+    if sys.argv[1] == "optimized":
+        run_experiment("--dump-states-on-halt=false --max-time=5min", ['1', '2', '3', '4'],
+                       klee_optimized_experiment)
+    else:
+        max_depths = list(map(str, range(1, 21))) + list(map(str, range(30, 110, 10)))
+        run_experiment("--dump-states-on-halt=false --max-time=5min", max_depths, klee_experiment)
 
 
 if __name__ == "__main__":
