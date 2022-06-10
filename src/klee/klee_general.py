3a336d9371691d1840da02ede3bc145147940633
Shathebean <shaheen@gmail.com>
2022-06-10 11:27:29 -0700

    Fixed a pycparser error

--- src/klee/klee_general.py
+++ src/klee/klee_general.py
@@ -1,5 +1,7 @@
 # For some inexplicable reason, run this with PYTHONPATH=$PYTHONPATH:/app/pycparser/ at the front
 """Script for running Klee on a series of functions and saving data."""
+import sys
+sys.path.append('/app/pycparser/')
 import subprocess
 import time
 import re
@@ -203,13 +205,15 @@ def main() -> None:
     # to look at later: 'active-object', 'amb', 'anagrams-1', 'anagrams-deranged-anagrams', 'animate-a-pendulum'
     functions = ['9-billion-names-of-god-the-integer', '24-game-solve', '24-game', 'abc-problem', 'ackermann-function-1', 'ackermann-function-2',
     'aks-test-for-primes', 'aliquot-sequence-classifications-1', 'aliquot-sequence-classifications-2', 'almost-prime', 'anagrams-2']
+    functions = ['24-game-solve']
+    # functions = ['simpletest']
     labels = ["normal"] # the labels for the different "compilation methods"
     # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14',
     #               '15', '16', '17', '18', '19', '20', '30', '40', '50', '60', '70', '80', '90',
     #               '100'] # all possible values for the input variable
     # xaxis = ['1', '2', '5']
     xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '20', '25']
-    # xaxis = ['1', '2']
+    xaxis = ['1', '2', '3']
     fields = ["ICov(%)", 'BCov(%)', "CompletedPaths", "GeneratedTests", "RealTime", "UserTime",
               "SysTime", "PythonTime"] # all klee output fields that we are interested in
     remove = True # whether klee files should be deleted after the important data is collected. Usually set to True
