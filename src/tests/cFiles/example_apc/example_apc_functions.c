52b5d70ead2572bd786702f04374e7e055e32821
Duy Lam <tomqlam@gmail.com>
2022-11-13 09:18:28 -0800

    uncomment some stuff

--- src/tests/cFiles/example_apc/example_apc_functions.c
+++ src/tests/cFiles/example_apc/example_apc_functions.c
@@ -175,19 +175,19 @@ int vector_product_rec(int A[], int B[], int n)
 }
 
 // function that multiplies two 2d matrices
-// int* matrix_product_iter(int A[], int B[], int n)
-// {
-//     int* product = malloc(n * n * sizeof(int));
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 0; j < n; j++) {
-//             product[i * n + j] = 0;
-//             for (int k = 0; k < n; k++) {
-//                 product[i * n + j] = product[i * n + j] + A[i * n + k] * B[k * n + j];
-//             }
-//         }
-//     }
-//     return product;
-// }
+int* matrix_product_iter(int A[], int B[], int n)
+{
+    int* product = (int*)malloc(n * n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            product[i * n + j] = 0;
+            for (int k = 0; k < n; k++) {
+                product[i * n + j] = product[i * n + j] + A[i * n + k] * B[k * n + j];
+            }
+        }
+    }
+    return product;
+}
 
 // function that finds the approximation of a n-root of a number using newton's method
 double newtons_method_rec(double number, int root, double guess)
@@ -221,107 +221,107 @@ double newtons_method_iter(double number, int root, double guess)
 }
 
 // function that find the longest common subsequence of two strings
-// char* longest_common_subsequence(char* a, char* b, int a_len, int b_len)
-// {
-//     // create a dp table to store the longest common subsequence
-//     int dp[a_len + 1][b_len + 1];
-//     // initialize the dp table
-//     for (int i = 0; i < a_len + 1; i++) {
-//         for (int j = 0; j < b_len + 1; j++) {
-//             dp[i][j] = 0;
-//         }
-//     }
-//     // fill the dp table
-//     for (int i = 1; i < a_len + 1; i++) {
-//         for (int j = 1; j < b_len + 1; j++) {
-//             if (a[i - 1] == b[j - 1]) {
-//                 // if the characters are the same, add 1 to the previous value to store the length of the longest common subsequence
-//                 dp[i][j] = dp[i - 1][j - 1] + 1;
-//             } else {
-//                 // if the characters are not equal, take the max of the previous row and column
-//                 dp[i][j] = dp[i - 1][j] > dp[i][j - 1] ? dp[i - 1][j] : dp[i][j - 1];
-//             }
-//         }
-//     }
-//     // create a string to store the longest common subsequence
-//     char* lcs = malloc((dp[a_len][b_len] + 1) * sizeof(char));
-//     // initialize the string
-//     for (int i = 0; i < dp[a_len][b_len] + 1; i++) {
-//         // set all characters to null
-//         lcs[i] = '\0';
-//     }
-//     // fill the string
-//     int i = a_len;
-//     int j = b_len;
-//     int k = dp[a_len][b_len];
-//     while (i > 0 && j > 0) {
-//         if (a[i - 1] == b[j - 1]) {
-//             lcs[k - 1] = a[i - 1];
-//             i--;
-//             j--;
-//             k--;
-//         } else if (dp[i - 1][j] > dp[i][j - 1]) {
-//             i--;
-//         } else {
-//             j--;
-//         }
-//     }
-//     return lcs;
-// }
+char* longest_common_subsequence(char* a, char* b, int a_len, int b_len)
+{
+    // create a dp table to store the longest common subsequence
+    int dp[a_len + 1][b_len + 1];
+    // initialize the dp table
+    for (int i = 0; i < a_len + 1; i++) {
+        for (int j = 0; j < b_len + 1; j++) {
+            dp[i][j] = 0;
+        }
+    }
+    // fill the dp table
+    for (int i = 1; i < a_len + 1; i++) {
+        for (int j = 1; j < b_len + 1; j++) {
+            if (a[i - 1] == b[j - 1]) {
+                // if the characters are the same, add 1 to the previous value to store the length of the longest common subsequence
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                // if the characters are not equal, take the max of the previous row and column
+                dp[i][j] = dp[i - 1][j] > dp[i][j - 1] ? dp[i - 1][j] : dp[i][j - 1];
+            }
+        }
+    }
+    // create a string to store the longest common subsequence
+    char* lcs = (char*)malloc((dp[a_len][b_len] + 1) * sizeof(char));
+    // initialize the string
+    for (int i = 0; i < dp[a_len][b_len] + 1; i++) {
+        // set all characters to null
+        lcs[i] = '\0';
+    }
+    // fill the string
+    int i = a_len;
+    int j = b_len;
+    int k = dp[a_len][b_len];
+    while (i > 0 && j > 0) {
+        if (a[i - 1] == b[j - 1]) {
+            lcs[k - 1] = a[i - 1];
+            i--;
+            j--;
+            k--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+    return lcs;
+}
 
-// // function that finds the longest common substring of two strings
-// char* longest_common_substring(char* a, char* b, int a_len, int b_len)
-// {
-//     int dp[a_len + 1][b_len + 1];
-//     // initialize the dp table
-//     for (int i = 0; i < a_len + 1; i++) {
-//         for (int j = 0; j < b_len + 1; j++) {
-//             dp[i][j] = 0;
-//         }
-//     }
-//     // fill the dp table
-//     int max = 0;
-//     int max_i = 0;
-//     int max_j = 0;
-//     for (int i = 1; i < a_len + 1; i++) {
-//         for (int j = 1; j < b_len + 1; j++) {
-//             if (a[i - 1] == b[j - 1]) {
-//                 // if the characters are the same, add 1 to the previous value to store the length of the longest common substring
-//                 dp[i][j] = dp[i - 1][j - 1] + 1;
-//                 if (dp[i][j] > max) {
-//                     max = dp[i][j];
-//                     max_i = i;
-//                     max_j = j;
-//                 }
-//             } else {
-//                 // if the characters are not equal, set the value to 0
-//                 dp[i][j] = 0;
-//             }
-//         }
-//     }
-//     // create a string to store the longest common substring
-//     char* lcs = malloc((max + 1) * sizeof(char));
-//     // initialize the string
-//     for (int i = 0; i < max + 1; i++) {
-//         // set all characters to null
-//         lcs[i] = '\0';
-//     }
-//     // fill the string
-//     int i = max_i;
-//     int j = max_j;
-//     int k = max;
-//     while (i > 0 && j > 0) {
-//         if (a[i - 1] == b[j - 1]) {
-//             lcs[k - 1] = a[i - 1];
-//             i--;
-//             j--;
-//             k--;
-//         } else {
-//             break;
-//         }
-//     }
-//     return lcs;
-// }
+// function that finds the longest common substring of two strings
+char* longest_common_substring(char* a, char* b, int a_len, int b_len)
+{
+    int dp[a_len + 1][b_len + 1];
+    // initialize the dp table
+    for (int i = 0; i < a_len + 1; i++) {
+        for (int j = 0; j < b_len + 1; j++) {
+            dp[i][j] = 0;
+        }
+    }
+    // fill the dp table
+    int max = 0;
+    int max_i = 0;
+    int max_j = 0;
+    for (int i = 1; i < a_len + 1; i++) {
+        for (int j = 1; j < b_len + 1; j++) {
+            if (a[i - 1] == b[j - 1]) {
+                // if the characters are the same, add 1 to the previous value to store the length of the longest common substring
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > max) {
+                    max = dp[i][j];
+                    max_i = i;
+                    max_j = j;
+                }
+            } else {
+                // if the characters are not equal, set the value to 0
+                dp[i][j] = 0;
+            }
+        }
+    }
+    // create a string to store the longest common substring
+    char* lcs = (char*)malloc((max + 1) * sizeof(char));
+    // initialize the string
+    for (int i = 0; i < max + 1; i++) {
+        // set all characters to null
+        lcs[i] = '\0';
+    }
+    // fill the string
+    int i = max_i;
+    int j = max_j;
+    int k = max;
+    while (i > 0 && j > 0) {
+        if (a[i - 1] == b[j - 1]) {
+            lcs[k - 1] = a[i - 1];
+            i--;
+            j--;
+            k--;
+        } else {
+            break;
+        }
+    }
+    return lcs;
+}
 
 void insertionSort(int arr[], int n)
 {
@@ -397,43 +397,43 @@ void quickSort(int A[], int n)
 }
 
 // function that performs mergesort on array
-// void mergeSort(int A[], int n)
-// {
-//     if (n <= 1) {
-//         return;
-//     }
-//     int m = n / 2;
-//     mergeSort(A, m);
-//     mergeSort(A + m, n - m);
-//     int* temp = malloc(n * sizeof(int));
-//     int i = 0;
-//     int j = m;
-//     int k = 0;
-//     while (i < m && j < n) {
-//         if (A[i] < A[j]) {
-//             temp[k] = A[i];
-//             i++;
-//         } else {
-//             temp[k] = A[j];
-//             j++;
-//         }
-//         k++;
-//     }
-//     while (i < m) {
-//         temp[k] = A[i];
-//         i++;
-//         k++;
-//     }
-//     while (j < n) {
-//         temp[k] = A[j];
-//         j++;
-//         k++;
-//     }
-//     for (int i = 0; i < n; i++) {
-//         A[i] = temp[i];
-//     }
-//     free(temp);
-// }
+void mergeSort(int A[], int n)
+{
+    if (n <= 1) {
+        return;
+    }
+    int m = n / 2;
+    mergeSort(A, m);
+    mergeSort(A + m, n - m);
+    int* temp = (int*)malloc(n * sizeof(int));
+    int i = 0;
+    int j = m;
+    int k = 0;
+    while (i < m && j < n) {
+        if (A[i] < A[j]) {
+            temp[k] = A[i];
+            i++;
+        } else {
+            temp[k] = A[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < m) {
+        temp[k] = A[i];
+        i++;
+        k++;
+    }
+    while (j < n) {
+        temp[k] = A[j];
+        j++;
+        k++;
+    }
+    for (int i = 0; i < n; i++) {
+        A[i] = temp[i];
+    }
+    free(temp);
+}
 
 // function that implements ackermann's function
 int ackermann_rec(int m, int n)
