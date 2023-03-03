22a6eaf46ce661d2e57e67af2d7d2cba3d5fd4ac
David Chen <davidc3287@gmail.com>
2023-03-02 16:55:50 -0800

    add iseven and isodd

--- src/experiments/function_calls/files/fcn_calls.c
+++ src/experiments/function_calls/files/fcn_calls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int fact(int n)
 {
@@ -18,154 +19,172 @@ int fact_wrapper(int n)
     return fact(n);
 }
 
-int gcd(int a, int b)
-{
-    while (a != b)
-    {
-        if (a > b)
-        {
-            a = a - b;
-        }
-        else
-        {
-            b = b - a;
-        }
-    }
-    return a;
-}
+// int gcd(int a, int b)
+// {
+//     while (a != b)
+//     {
+//         if (a > b)
+//         {
+//             a = a - b;
+//         }
+//         else
+//         {
+//             b = b - a;
+//         }
+//     }
+//     return a;
+// }
 
-int gcd_wrapper(int a, int b)
-{
-    return gcd(a, b);
-}
+// int gcd_wrapper(int a, int b)
+// {
+//     return gcd(a, b);
+// }
 
-void merge(int arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+// void merge(int arr[], int l, int m, int r)
+// {
+//     int i, j, k;
+//     int n1 = m - l + 1;
+//     int n2 = r - m;
 
-    // create temporary arrays
-    int L[n1], R[n2];
+//     // create temporary arrays
+//     int L[n1], R[n2];
 
-    // copy data to temp arrays L[] and R[]
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
+//     // copy data to temp arrays L[] and R[]
+//     for (i = 0; i < n1; i++)
+//         L[i] = arr[l + i];
+//     for (j = 0; j < n2; j++)
+//         R[j] = arr[m + 1 + j];
 
-    // merge the temp arrays back into arr[l..r]
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
+//     // merge the temp arrays back into arr[l..r]
+//     i = 0;
+//     j = 0;
+//     k = l;
+//     while (i < n1 && j < n2)
+//     {
+//         if (L[i] <= R[j])
+//         {
+//             arr[k] = L[i];
+//             i++;
+//         }
+//         else
+//         {
+//             arr[k] = R[j];
+//             j++;
+//         }
+//         k++;
+//     }
 
-    // copy the remaining elements of L[], if there are any
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+//     // copy the remaining elements of L[], if there are any
+//     while (i < n1)
+//     {
+//         arr[k] = L[i];
+//         i++;
+//         k++;
+//     }
 
-    // copy the remaining elements of R[], if there are any
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
+//     // copy the remaining elements of R[], if there are any
+//     while (j < n2)
+//     {
+//         arr[k] = R[j];
+//         j++;
+//         k++;
+//     }
+// }
 
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r)
-    {
-        int m = l + (r - l) / 2;
+// void mergeSort(int arr[], int l, int r)
+// {
+//     if (l < r)
+//     {
+//         int m = l + (r - l) / 2;
 
-        // sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
+//         // sort first and second halves
+//         mergeSort(arr, l, m);
+//         mergeSort(arr, m + 1, r);
 
-        // merge the sorted halves
-        merge(arr, l, m, r);
-    }
-}
+//         // merge the sorted halves
+//         merge(arr, l, m, r);
+//     }
+// }
 
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+// void swap(int *a, int *b)
+// {
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
 
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high];
-    int i = (low - 1);
+// int partition(int arr[], int low, int high)
+// {
+//     int pivot = arr[high];
+//     int i = (low - 1);
 
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
+//     for (int j = low; j <= high - 1; j++)
+//     {
+//         if (arr[j] < pivot)
+//         {
+//             i++;
+//             swap(&arr[i], &arr[j]);
+//         }
+//     }
+//     swap(&arr[i + 1], &arr[high]);
+//     return (i + 1);
+// }
 
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
+// void quickSort(int arr[], int low, int high)
+// {
+//     if (low < high)
+//     {
+//         int pi = partition(arr, low, high);
+//         quickSort(arr, low, pi - 1);
+//         quickSort(arr, pi + 1, high);
+//     }
+// }
 
-// calls fact_wrapper -> calls fact
-int sum_factorials(int n)
-{
-    int sum = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        sum += fact_wrapper(i);
-    }
-    return sum;
-}
+// // calls fact_wrapper -> calls fact
+// int sum_factorials(int n)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         sum += fact_wrapper(i);
+//     }
+//     return sum;
+// }
 
-int main()
-{
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
+// bool is_even(int n) {
+//     if (n == 0) {
+//         return true;
+//     }
+//     else {
+//         return is_odd(n - 1);
+//     }
+// }
 
-    printf("Given array is \n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+// bool is_odd(int n) {
+//     if (n == 0) {
+//         return false;
+//     }
+//     else {
+//         return is_even(n - 1);
+//     }
+// }
 
-    // mergeSort(arr, 0, n - 1);
-    quickSort(arr, 0, n - 1);
+// int main()
+// {
+//     int arr[] = {12, 11, 13, 5, 6, 7};
+//     int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Sorted array is \n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-    return 0;
-}
+//     printf("Given array is \n");
+//     for (int i = 0; i < n; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+
+//     // mergeSort(arr, 0, n - 1);
+//     quickSort(arr, 0, n - 1);
+
+//     printf("Sorted array is \n");
+//     for (int i = 0; i < n; i++)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+//     return 0;
+// }
