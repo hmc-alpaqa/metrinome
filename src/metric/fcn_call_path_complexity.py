2567d57f9e985b6b7140d93ac70b03482ef0486e
David Chen <davidc3287@gmail.com>
2023-03-05 18:11:13 -0800

    multi fact test

--- src/metric/fcn_call_path_complexity.py
+++ src/metric/fcn_call_path_complexity.py
@@ -251,15 +251,18 @@ class FunctionCallPathComplexity(ABC):
 
         init_eqns = [symbols(f'V{i}_0')*x - init_nodes[i] for i in range(num_cfgs)]
         symbs = init_nodes + symbs
-        print('SYSTEM', init_eqns + system)
+        full_sys = init_eqns + system
         T0, T1, V0_0, V0_1, V1_0, V1_1, V1_2, V1_3, V1_4, V1_5, V1_6, V1_7, V1_8, V1_9, V1_10, V1_11, V1_12, V1_13, V1_14, V1_15, V1_16, V1_17, V1_18, V1_19, V1_20, V1_21, x = symbols('T0 T1 V0_0 V0_1 V1_0 V1_1 V1_2 V1_3 V1_4 V1_5 V1_6 V1_7 V1_8 V1_9 V1_10 V1_11 V1_12 V1_13 V1_14 V1_15 V1_16 V1_17 V1_18 V1_19 V1_20 V1_21 x')
 
         # sys_ = [-T0 + V0_0*x, -T1 + V1_0*x, -V0_0 + V0_1*x + x, T0**2*T1*x - V0_1, -V1_0 + V1_1*x, -V1_1 + V1_2*x + V1_4*x, -V1_2 + V1_3*x, V1_1*x - V1_3, -V1_4 + V1_5*x, -V1_5 + V1_6*x + V1_8*x, -V1_6 + V1_7*x, V1_5*x - V1_7, -V1_8 + V1_9*x, V1_10*x + V1_11*x - V1_9, -V1_10 + V1_11*x, -V1_11 + V1_12*x + V1_16*x, -V1_12 + V1_13*x + V1_14*x, -V1_13 + V1_15*x, -V1_14 + V1_15*x, -V1_15 + V1_9*x, -V1_16 + V1_17*x, -V1_17 + V1_18*x + V1_19*x, V1_17*x - V1_18, -V1_19 + V1_20*x, -V1_20 + V1_21*x + x, V1_20*x - V1_21]
 
 
         # sys_ = [-T0 + V0_0*x, -T1 + V1_0*x, -V0_0 + V0_1*x + x, T0*x + T1*x - V0_1, -V1_0 + V1_1*x, -V1_1 + V1_2*x + V1_4*x, -V1_2 + V1_3*x, V1_1*x - V1_3, -V1_4 + V1_5*x, -V1_5 + V1_6*x + V1_8*x, -V1_6 + V1_7*x, V1_5*x - V1_7, -V1_8 + V1_9*x, V1_10*x + V1_11*x - V1_9, -V1_10 + V1_11*x, -V1_11 + V1_12*x + V1_16*x, -V1_12 + V1_13*x + V1_14*x, -V1_13 + V1_15*x, -V1_14 + V1_15*x, -V1_15 + V1_9*x, -V1_16 + V1_17*x, -V1_17 + V1_18*x + V1_19*x, V1_17*x - V1_18, -V1_19 + V1_20*x, -V1_20 + V1_21*x + x, V1_20*x - V1_21]
-        gamma = sympy.expand(self.eliminate(init_eqns + system, symbs))
-        # gamma = sympy.expand(self.eliminate(sys_, symbs))
+
+        full_sys = [-T0 + V0_0*x, -T1 + V1_0*x, 2*T1*V0_1*x - V0_0, -V0_1 + x, -V1_0 + V1_1*x + V1_2*x, -V1_1 + x, T1*x - V1_2]
+        print('SYSTEM', full_sys)
+
+        gamma = sympy.expand(self.eliminate(full_sys, symbs))
         return gamma
 
 
