f26d14a0c526c0ec96bf6b9835edcd5a4aa2e8a9
David Chen <davidc3287@gmail.com>
2023-04-07 11:56:00 -0700

    clean up code, remove resultant and use built in for discrim

--- src/experiments/function_calls/files/fcn_calls.c
+++ src/experiments/function_calls/files/fcn_calls.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// bool is_even(int n);
-// bool is_odd(int n);
+bool is_even(int n);
+bool is_odd(int n);
 int max_value_iter(int arr[], int n);
 void merge(int arr[], int l, int m, int r);
 int fact(int n);
@@ -13,49 +13,49 @@ int fact_wrapper(int n);
 int fib_iter(int n);
 int fib_rec(int n);
 
-// int fact(int n)
-// {
-//     if (n == 0)
-//     {
-//         return 1;
-//     }
-//     else
-//     {
-//         return n * fact(n - 1);
-//     }
-// }
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
 
-// void fcn_medley(int n)
-// {
-//     fact(n);
-//     fib_iter(n);
-//     fib_rec(n);
-//     fact_wrapper(n);
-// }
+void fcn_medley(int n)
+{
+    fact(n);
+    fib_iter(n);
+    fib_rec(n);
+    fact_wrapper(n);
+}
 
-// int fact_wrapper(int n)
-// {
-//     return fact(n);
-// }
+int fact_wrapper(int n)
+{
+    return fact(n);
+}
 
 // int fact_wrapper_2x(int n)
 // {
 //     return fact(n) + fact(n);
 // }
 
-// int fib_iter(int n)
-// {
-//     int a = 0;
-//     int b = 1;
-//     int f;
-//     for (int i = 0; i < n; i++)
-//     {
-//         f = a + b;
-//         a = b;
-//         b = f;
-//     }
-//     return a;
-// }
+int fib_iter(int n)
+{
+    int a = 0;
+    int b = 1;
+    int f;
+    for (int i = 0; i < n; i++)
+    {
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    return a;
+}
 
 int fib_rec(int n)
 {
@@ -92,140 +92,140 @@ void mergeSortSimple(int arr[], int l, int r)
 //     return fact(n) + fact(n) + fact(n);
 // }
 
-// int gcd(int a, int b)
-// {
-//     while (a != b)
-//     {
-//         if (a > b)
-//         {
-//             a = a - b;
-//         }
-//         else
-//         {
-//             b = b - a;
-//         }
-//     }
-//     return a;
-// }
+int gcd(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+    return a;
+}
 
-// int gcd_wrapper(int a, int b)
-// {
-//     return gcd(a, b);
-// }
+int gcd_wrapper(int a, int b)
+{
+    return gcd(a, b);
+}
 
-// void merge(int arr[], int l, int m, int r)
-// {
-//     int i, j, k;
-//     int n1 = m - l + 1;
-//     int n2 = r - m;
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-//     // create temporary arrays
-//     int L[n1], R[n2];
+    // create temporary arrays
+    int L[n1], R[n2];
 
-//     // copy data to temp arrays L[] and R[]
-//     for (i = 0; i < n1; i++)
-//         L[i] = arr[l + i];
-//     for (j = 0; j < n2; j++)
-//         R[j] = arr[m + 1 + j];
+    // copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
-//     // merge the temp arrays back into arr[l..r]
-//     i = 0;
-//     j = 0;
-//     k = l;
-//     while (i < n1 && j < n2)
-//     {
-//         if (L[i] <= R[j])
-//         {
-//             arr[k] = L[i];
-//             i++;
-//         }
-//         else
-//         {
-//             arr[k] = R[j];
-//             j++;
-//         }
-//         k++;
-//     }
+    // merge the temp arrays back into arr[l..r]
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
 
-//     // copy the remaining elements of L[], if there are any
-//     while (i < n1)
-//     {
-//         arr[k] = L[i];
-//         i++;
-//         k++;
-//     }
+    // copy the remaining elements of L[], if there are any
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
 
-//     // copy the remaining elements of R[], if there are any
-//     while (j < n2)
-//     {
-//         arr[k] = R[j];
-//         j++;
-//         k++;
-//     }
-// }
+    // copy the remaining elements of R[], if there are any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
 
-// void mergeSort(int arr[], int l, int r)
-// {
-//     if (l < r)
-//     {
-//         int m = l + (r - l) / 2;
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
 
-//         // sort first and second halves
-//         mergeSort(arr, l, m);
-//         mergeSort(arr, m + 1, r);
+        // sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
 
-//         // merge the sorted halves
-//         merge(arr, l, m, r);
-//     }
-// }
+        // merge the sorted halves
+        merge(arr, l, m, r);
+    }
+}
 
-// void mergeSortSingleFcn(int A[], int n)
-// {
-//     if (n <= 1)
-//     {
-//         return;
-//     }
-//     int m = n / 2;
-//     mergeSortSingleFcn(A, m);
-//     mergeSortSingleFcn(A + m, n - m);
-//     int *temp = (int *)malloc(n * sizeof(int));
-//     int i = 0;
-//     int j = m;
-//     int k = 0;
-//     while (i < m && j < n)
-//     {
-//         if (A[i] < A[j])
-//         {
-//             temp[k] = A[i];
-//             i++;
-//         }
-//         else
-//         {
-//             temp[k] = A[j];
-//             j++;
-//         }
-//         k++;
-//     }
-//     while (i < m)
-//     {
-//         temp[k] = A[i];
-//         i++;
-//         k++;
-//     }
-//     while (j < n)
-//     {
-//         temp[k] = A[j];
-//         j++;
-//         k++;
-//     }
-//     for (int i = 0; i < n; i++)
-//     {
-//         A[i] = temp[i];
-//     }
-//     free(temp);
+void mergeSortSingleFcn(int A[], int n)
+{
+    if (n <= 1)
+    {
+        return;
+    }
+    int m = n / 2;
+    mergeSortSingleFcn(A, m);
+    mergeSortSingleFcn(A + m, n - m);
+    int *temp = (int *)malloc(n * sizeof(int));
+    int i = 0;
+    int j = m;
+    int k = 0;
+    while (i < m && j < n)
+    {
+        if (A[i] < A[j])
+        {
+            temp[k] = A[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = A[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < m)
+    {
+        temp[k] = A[i];
+        i++;
+        k++;
+    }
+    while (j < n)
+    {
+        temp[k] = A[j];
+        j++;
+        k++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = temp[i];
+    }
+    free(temp);
 
-//     return;
-// }
+    return;
+}
 
 // void swap(int *a, int *b)
 // {
@@ -261,40 +261,40 @@ void mergeSortSimple(int arr[], int l, int r)
 //     }
 // }
 
-// // calls fact_wrapper -> calls fact
-// int sum_factorials(int n)
-// {
-//     int sum = 0;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         sum += fact_wrapper(i);
-//     }
-//     return sum;
-// }
+// calls fact_wrapper -> calls fact
+int sum_factorials(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += fact_wrapper(i);
+    }
+    return sum;
+}
 
-// bool is_even(int n)
-// {
-//     if (n == 0)
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         return is_odd(n - 1);
-//     }
-// }
+bool is_even(int n)
+{
+    if (n == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return is_odd(n - 1);
+    }
+}
 
-// bool is_odd(int n)
-// {
-//     if (n == 0)
-//     {
-//         return false;
-//     }
-//     else
-//     {
-//         return is_even(n - 1);
-//     }
-// }
+bool is_odd(int n)
+{
+    if (n == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return is_even(n - 1);
+    }
+}
 
 // int max_value_iter(int A[], int n)
 // {
