fe9b891e97d5090323acdb3e24b15f381ed4b5c2
mkaniyur <mkaniyur@g.hmc.edu>
2023-05-30 16:19:27 -0700

    Show graphs in simple pipeline.

--- src/experiments/elimination/eliminateoptimize.py
+++ src/experiments/elimination/eliminateoptimize.py
@@ -13,36 +13,41 @@ from sympy import (Basic, Float, Matrix, Poly, degree, eye, preorder_traversal, 
 
 class Eliminator:
 
-    def __init(self, systems = []):
+    def __init__(self, systems = []):
         self.systems = systems
-        self.fullsystem = [eqn for eqn in system for system in self.systems]
+        self.fullsystem = [eqn for system in self.systems for eqn in system]
         self.symbs = []
         self.fullsymbs = set()
-        #for system in self.systems:
-        #    systemsymbs = set()
-        #    for eqn in system:
+        for system in self.systems:
+            systemsymbs = set()
+            for eqn in system:
+                eqn.remove("-")
+                eqn.remove("+")
+                eqn.remove("*x")
+                eqn.split(" ")
+                systemsymbs += eqn.free_symbols
+                self.fullsymbs += eqn.free_symbols
+            self.symbs += systemsymbs
 
-                
+    def simpleEliminate(self):
+        """Takes in a system of equations and gets the gamma function"""
+        if len(self.fullsystem) == 1:
+            return self.fullsystem[0]
+        sub = self.fullsystem[-1] + self.fullsymbs[-1]
+        if self.fullsymbs[-1] in sub.free_symbols:
+            for eq in self.fullsystem:
+                if self.fullsymbs[-1] in eq.free_symbols:
+                    sol = sympy.solve(eq, self.fullsymbs[-1], dict=True)
+                    if len(sol) == 1:
+                        sub = sympy.expand(sub.subs(self.fullsymbs[-1], sol[0][self.fullsymbs[-1]]))
+        if symbs[-1] in sub.free_symbols:
+            print("PANIC PANIC not sure how to substitute.")
+        for count, eq in enumerate(self.fullsystem):
+            if self.fullsymbs[-1] in eq.free_symbols:
+                self.fullsystem[count] = sympy.expand(eq.subs(self.fullsymbs[-1], sub))
+        return sympy.expand(self.simpleEliminate(self.fullsystem[:-1], self.fullsymbs[:-1]))
 
-    def simpleEliminate(self, system, symbs):
-    """Takes in a system of equations and gets the gamma function"""
-    if len(system) == 1:
-        return system[0]
-    sub = system[-1] + symbs[-1]
-    if symbs[-1] in sub.free_symbols:
-        for eq in system:
-            if symbs[-1] in eq.free_symbols:
-                sol = sympy.solve(eq, symbs[-1], dict=True)
-                if len(sol) == 1:
-                    sub = sympy.expand(sub.subs(symbs[-1], sol[0][symbs[-1]]))
-    if symbs[-1] in sub.free_symbols:
-        print("PANIC PANIC not sure how to substitute.")
-    for count, eq in enumerate(system):
-        if symbs[-1] in eq.free_symbols:
-            system[count] = sympy.expand(eq.subs(symbs[-1], sub))
-    return sympy.expand(self.simpleEliminate(system[:-1], symbs[:-1]))
-
-    def optimizedEliminate(self, system, symbs):
+    def optimizedEliminate(self):
         """Takes in a system of equations and gets the gamma function"""
         if len(system) == 1:
             return system[0]
@@ -60,3 +65,10 @@ class Eliminator:
                 system[count] = sympy.expand(eq.subs(symbs[-1], sub))
         return sympy.expand(self.optimizedEliminate(system[:-1], symbs[:-1]))
 
+def main():
+    systems = [["-T0 + V0_0*x", "-V0_0 + V0_1*x + V0_2*x", "-V0_1 + x", "T1*x - V0_2"], ["-T1 + V1_0*x", "-V1_0 + V1_1*x + V1_2*x", "-V1_1 + x", "T0*x - V1_2"]]
+    elim = Eliminator(systems)
+    elim.simpleEliminate()
+
+if __name__ == "__main__":
+    main()
