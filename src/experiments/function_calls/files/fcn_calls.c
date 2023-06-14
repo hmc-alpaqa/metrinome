d9b87821ca2cfb966a76cc8f3b96820ece99ab58
mkaniyur <mkaniyur@g.hmc.edu>
2023-06-14 15:21:29 -0700

    Testing + debugging integrated elim + linalg solve

--- src/experiments/function_calls/files/fcn_calls.c
+++ src/experiments/function_calls/files/fcn_calls.c
@@ -43,6 +43,35 @@ int mul_inv(int a, int b)
     return x1;
 }
 
+int gcd(int a, int b)
+{
+    if (n % 2 == 0)
+    {
+        return fib_rec(n); // exp
+    }
+    else
+    {
+        return gcd(n, 24); // exp
+    }
+}
+
+int mul_inv(int a, int b)
+{
+    int b0 = b, t, q;
+    int x0 = 0, x1 = 1;
+    if (b == 1)
+        return 1;
+    while (a > 1)
+    {
+        q = a / b;
+        t = b, b = a % b, a = t;
+        t = x0, x0 = x1 - q * x0, x1 = t;
+    }
+    if (x1 < 0)
+        x1 += b0;
+    return x1;
+}
+
 // int gcd(int a, int b)
 // {
 //     while (a != b)
