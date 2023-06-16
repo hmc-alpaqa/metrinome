b5042b51654bd8e4221807b1c7e917c30f5212a1
YukiYang31 <yangyihan121@gmail.com>
2023-06-16 13:39:22 -0700

    test for new fc apc

---
+++ src/experiments/optimization/test_newNode_eliminate_msolve.py
@@ -0,0 +1,175 @@
+"""compare the regular path complexity, recursive path complexity, new fcn calls path complexity, and their run time for 
+   files in experiments/optimization/files.txt"""
+from utils import Timeout
+from metric.path_complexity import PathComplexityRes
+from metric import cyclomatic_complexity, npath_complexity, path_complexity, fcn_call_path_complexity, recursive_path_complexity
+from lang_to_cfg.cpp import CPPConvert
+from core.log import Log, LogLevel
+import pandas as pd  # type: ignore
+from typing import Union
+import time
+import os
+import sys
+import fc_path_complexity_eliminate
+from sympy import Number
+
+class DataCollector:
+    """Compute and store all complexity metrics and timing data."""
+
+    def __init__(self) -> None:
+        """Create a new instance of the data collector."""
+        log = Log(log_level=LogLevel.DEBUG)
+        # log = Log(log_level=LogLevel.REGULAR)
+        self.converter = CPPConvert(log)
+        self.apc_computer = path_complexity.PathComplexity(log)
+        self.fcn_call_apc_new_computer = fc_path_complexity_eliminate.FunctionCallPathComplexity(log)
+        self.recursive_apc_computer = recursive_path_complexity.RecursivePathComplexity(log)
+        self.cyclo_computer = cyclomatic_complexity.CyclomaticComplexity(log)
+        self.npath_computer = npath_complexity.NPathComplexity(log)
+        self.base_path = "/app/code/experiments/function_calls/files/"
+
+    # nfcapc stands for new function call apc, which is the apc computed by fc_path_complexity_final
+    # pylint: disable=broad-except
+    def collect(self) -> None:
+        """Compute the metrics for all files and store the data."""
+        data = pd.DataFrame({"file_name": [], "graph_name": [], "apc": [], "rapc": [],  "nfcapc": [],
+                             "cyclo": [], "npath": [], "apc_time": [], "rapc_time": [], "nfcapc_time": [],
+                             "num_vertices": [], "edge_count": [], "exception": [],
+                             "exception_type": []})
+        with open('/app/code/experiments/optimization/files.txt') as funcs:
+            # files = ['/app/code/experiments/recursion/files/catalan-numbers-1.c' ]
+
+            files = [line.rstrip() for line in funcs]
+
+        for file in files:
+            print(f"Now analyzing {file}")
+            graphs = self.converter.to_graph(os.path.splitext(file)[0], ".c")
+            print(graphs)
+            if graphs is None:
+                graphs = self.converter.to_graph(
+                    os.path.splitext(file)[0], ".cpp")
+            if graphs is None:
+                print("No Graphs")
+                continue
+
+            for graph_name, graph in graphs.items():
+                # if graph_name != 'fcn_calls_cfg._Z9mergeSortPiii.dot':
+                # if graph_name != 'fcn_calls_cfg._Z18multi_fact_wrapperi.dot':
+                # if graph_name != 'fcn_calls_cfg._Z10fcn_medleyi.dot':
+                # if graph_name != 'fcn_calls_cfg._Z15mergeSortSimplePiii.dot':
+                    # continue
+                print('Graph Name: ', graph_name)
+                apc: Union[str, PathComplexityRes] = "na"
+                rapc: Union[str, PathComplexityRes] = "na"
+                nfcapc: Union[str, PathComplexityRes] = "na"
+                npath: Union[str, int] = "na"
+                cyclo: Union[str, int] = "na"
+                exception_type = "na"
+                runtime = 0.0
+                rruntime = 0.0
+                nfcruntime = 0.0
+                start_time = time.time()
+                try:
+                    with Timeout(2000):
+                        # if graph_name != 'fcn_calls_cfg._Z15mergeSortSimplePiii.dot':
+                        #     continue
+                        nfcapc = self.fcn_call_apc_new_computer.evaluate(graph, graphs)
+                        nfcruntime = time.time() - start_time
+                except Exception as exc:
+                    print(f"Exception: {exc}")
+                    exception_type = "Timeout" if isinstance(
+                        exc, TimeoutError) else "Other"
+
+                # start_time = time.time()
+                # try:
+                #     with Timeout(300):
+                #         recurlist = []
+                #         end = graph.graph.num_vertices - 1
+                #         for node in graph.metadata.calls.keys():
+                #             if graph.name.split(".")[1] in graph.metadata.calls[node]:
+                #                 recurlist += [int(node)]
+                #         oldEdges = graph.graph.edges
+                #         for node in recurlist:
+                #             graph.graph.edges = graph.graph.edges + [[node, 0]]
+                #             graph.graph.edges = graph.graph.edges + \
+                #                 [[end, node]]
+                #         brapc = self.apc_computer.evaluate(graph)
+                #         bruntime = time.time() - start_time
+                #         graph.graph.edges = oldEdges
+                # except Exception as exc:
+                #     print(f"Exception: {exc}")
+                #     exception_type = "Timeout" if isinstance(
+                #         exc, TimeoutError) else "Other"
+
+                start_time = time.time()
+                try:
+                    with Timeout(2000):
+                        rapc = self.recursive_apc_computer.evaluate(graph)
+                        rruntime = time.time() - start_time
+                except Exception as exc:
+                    print(f"Exception: {exc}")
+                    exception_type = "Timeout" if isinstance(
+                        exc, TimeoutError) else "Other"
+    
+                start_time = time.time()
+                try:
+                    with Timeout(300):
+                        apc = self.apc_computer.evaluate(graph)
+                        runtime = time.time() - start_time
+                except Exception as exc:
+                    exception_type = "Timeout" if isinstance(
+                        exc, TimeoutError) else "Other"
+                        
+                # try:
+                #     with Timeout(200):
+                #         cyclo = self.cyclo_computer.evaluate(graph)
+                # except Exception as exc:
+                #     exception_type = "Timeout" if isinstance(
+                #         exc, TimeoutError) else "Other"
+
+                # try:
+                #     with Timeout(200):
+                #         npath = self.npath_computer.evaluate(graph)
+                # except Exception as exc:
+                #     exception_type = "Timeout" if isinstance(
+                #         exc, TimeoutError) else "Other"
+
+                new_row = {"file_name": file, "graph_name": graph.name, "apc": apc,
+                           "rapc": rapc, "nfcapc": nfcapc, "cyclo": cyclo, "npath": npath,
+                           "apc_time": runtime, "rapc_time": rruntime, "nfcapc_time": nfcruntime,
+                           "num_vertices": graph.graph.num_vertices,
+                           "edge_count": graph.graph.edge_count(),
+                           "exception_type": exception_type}
+
+                data = data.append(new_row, ignore_index=True)
+                # only keep columns graph_name, rapc, fcapc, num_vertices, edge_count, and runtimes
+                data = data[["graph_name", "apc","rapc", "nfcapc", "apc_time","rapc_time","nfcapc_time"]]
+
+                # format rapc column decimals to have at most 3 decimal places, e.g. 0.33333333n -> 0.333n
+                # data['rapc'] = data['rapc'].apply(lambda x: round_tuple_of_exprs(x, 3))
+                # print(data[['graph_name', "apc",'rapc',"rapc_time","fcapc","fcapc_time"]])
+                print(data[["graph_name",  "apc","rapc", "nfcapc", "apc_time","rapc_time","nfcapc_time"]])
+
+
+
+                # create directory if it doesn't exist
+                if not os.path.exists("/app/code/experiments/optimization/data"):
+                    os.makedirs("/app/code/experiments/optimization/data")
+                data.to_csv(
+                    "/app/code/experiments/optimization/data/finalTestData.csv")
+
+def round_tuple_of_exprs(tup, num_digits):
+    return tuple(round_expr(expr, num_digits) for expr in tup)
+                
+def round_expr(expr, num_digits):
+    return expr.xreplace({n : round(n, num_digits) for n in expr.atoms(Number)})
+
+
+def main() -> None:
+    """Compute metrics for many graphs."""
+    data_collector = DataCollector()
+    data_collector.collect()
+
+
+if __name__ == "__main__":
+    main()
