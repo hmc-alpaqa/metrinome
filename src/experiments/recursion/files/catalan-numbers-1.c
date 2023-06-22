ce4914d756fb1e7e6ce90d520017485b11f55c21
YukiYang31 <yangyihan121@gmail.com>
2023-06-22 14:59:00 -0700

    m

--- src/experiments/recursion/files/catalan-numbers-1.c
+++ src/experiments/recursion/files/catalan-numbers-1.c
@@ -15,20 +15,20 @@ ull binomial(ull m, ull n)
 	return r;
 }
 
-// ull catalan1(int n) {
-// 	return binomial(2 * n, n) / (1 + n);
-// }
+ull catalan1(int n) {
+	return binomial(2 * n, n) / (1 + n);
+}
 
-// ull catalan2(int n) {
-// 	int i;
-// 	ull r = !n;
+ull catalan2(int n) {
+	int i;
+	ull r = !n;
 
-// 	for (i = 0; i < n; i++)
-// 		r += catalan2(i) * catalan2(n - 1 - i);
-// 	return r;
-// }
+	for (i = 0; i < n; i++)
+		r += catalan2(i) * catalan2(n - 1 - i);
+	return r;
+}
 
-// ull catalan3(int n)
-// {
-// 	return n ? 2 * (2 * n - 1) * catalan3(n - 1) / (1 + n) : 1;
-// }
+ull catalan3(int n)
+{
+	return n ? 2 * (2 * n - 1) * catalan3(n - 1) / (1 + n) : 1;
+}
