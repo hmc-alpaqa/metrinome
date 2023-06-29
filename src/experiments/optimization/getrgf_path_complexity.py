fe64b074b520394f00b4c32ae3dfc0f163f8ff79
astudart <astudart@g.hmc.edu>
2023-06-29 10:35:07 -0700

    get Ak (num and den)

--- src/experiments/optimization/getrgf_path_complexity.py
+++ src/experiments/optimization/getrgf_path_complexity.py
@@ -20,46 +20,72 @@ from graph.control_flow_graph import ControlFlowGraph
 from graph.graph import Graph
 from metric import metric
 from utils import Timeout, big_o, get_solution_from_roots, get_taylor_coeffs, calls_function
+import math
 
 PathComplexityRes = tuple[Union[float, str], Union[float, str]]
 
 PRECISION = 11
 
 
-""" Finds the root with the maximum rho (1/root) among those. This root 
-will originate the greatest APC.
-Input: rootsDict, dictionary of all roots of the generating function
-Return: maxrho and rhoDict
-"""
-def getRhoDict(rootsDict):
-    return 0
+# """ Finds the root with the maximum rho (1/root) among those. This root 
+# will originate the greatest APC.
+# Input: rootsDict, dictionary of all roots of the generating function
+# Return: maxrho and rhoDict
+# """
+# def getRhoDict(rootsDict):
+#     return 0
 
-"""
-Input: denominator
-Return: q0
-"""
-def getQ0(denominator):
-    return 0
+# """
+# Input: denominator
+# Return: q0
+# """
+# def getQ0(denominator):
+#     return 0
 
 
-""" Identifies other roots with same magnitude as the root with
-maximum APC.
-Input: rootsDict, rootsAPCDict
-Return: APC
-"""
-def getAPC(rhosDict, maxRho, q0, numerator):
-    return 0 
+# """ Identifies other roots with same magnitude as the root with
+# maximum APC.
+# Input: rootsDict, rootsAPCDict
+# Return: APC
+# """
+# def getAPC(rhoDict, maxRho, q0, numerator):
+#     return 0 
 
 """
 Input:
 Return: Denominator of Ak
 """
-def calculateAk(q0,rhok,rhosDict):
-    return 0
+def calculateAk(q0,rhok,rhoDict):
+    denominator = q0
+    mult = rhoDict[rhok]
+    denominator *= math.factorial(mult-1)
+    productory = 1
+    for rho in rhoDict:
+        if rho != rhok:
+            productory *= (1-rho/rhok)^rhoDict[rho]
+    denominator *= productory
+    return denominator
 
+
+# x = sympy.Symbol("x")
+# f = sympy.Function('f')(x)
+# equation = sympy.Eq(f,x**3+x**2)
+# numerator = sympy.solve(equation,f)
+# numerator = numerator[0]
+# rhok = 0.5
 """
 Input:
 Return:
 """
 def shiftAk(numerator, rhok):
-    return 0
+    rhokDict = {symbols("x"):1/rhok}
+    numeratorAK = numerator.subs(rhokDict)
+    # print(numeratorAK)
+    return numeratorAK
+
+def main():
+    shiftAk(numerator,rhok)
+
+
+if __name__ == "__main__":
+    main()
