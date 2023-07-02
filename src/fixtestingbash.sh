419ca60217e4e57aa077a627b448a82d0e34350f
YukiYang31 <yangyihan121@gmail.com>
2023-07-01 18:50:20 -0700

    added test_og

--- src/fixtestingbash.sh
+++ src/fixtestingbash.sh
@@ -16,6 +16,8 @@ python /app/code/tests/test_rapc.py
 
 python /app/code/tests/test_fcapc.py
 
+python /app/code/tests/test_og.py
+
 python /app/code/tests/test_nfcapc.py
 
 python /app/code/tests/test_getrgf.py #must run this in order for mergeTestResult to work
--- src/tests/mergeTestResult.py
+++ src/tests/mergeTestResult.py
@@ -3,16 +3,18 @@ import os
 
 
 apc_csv_file_path = '/app/code/tests/data/apc_data.csv'
-fcapc_csv_file_path = '/app/code/tests/data/fcapc_data.csv'
-nfcapc_csv_file_path = '/app/code/tests/data/nfcapc_data.csv'
 rapc_csv_file_path = '/app/code/tests/data/rapc_data.csv'
+fcapc_csv_file_path = '/app/code/tests/data/fcapc_data.csv'
+ogapc_csv_file_path = '/app/code/tests/data/ogapc_data.csv'
+nfcapc_csv_file_path = '/app/code/tests/data/nfcapc_data.csv'
 getrgfapc_csv_file_path = '/app/code/tests/data/getrgfapc_data.csv'
 
 if os.path.exists(getrgfapc_csv_file_path):
     getrgfapc_df = pd.read_csv(getrgfapc_csv_file_path)
     getrgfapc_df = getrgfapc_df.iloc[:,1:]
 else:
-    print('PANIC, cannnot merge')
+    raise CustomError("PANIC, no getrgf data, cannot proceed")
+    
 
 ###########Initializing dataframs##############
 apc_df = pd.DataFrame(columns = ['graph_name','apc','apc_time'])
@@ -27,6 +29,10 @@ fcapc_df = pd.DataFrame(columns = ['graph_name','fcapc','fcapc_time'])
 fcapc_df['graph_name'] = getrgfapc_df['graph_name']
 fcapc_df = fcapc_df.fillna('na')
 
+ogapc_df = pd.DataFrame(columns = ['graph_name','ogapc','ogapc_time'])
+ogapc_df['graph_name'] = getrgfapc_df['graph_name']
+ogapc_df = ogapc_df.fillna('na')
+
 nfcapc_df = pd.DataFrame(columns = ['graph_name','nfcapc','nfcapc_time'])
 nfcapc_df['graph_name'] = getrgfapc_df['graph_name']
 nfcapc_df = nfcapc_df.fillna('na')
@@ -36,7 +42,7 @@ if os.path.exists(apc_csv_file_path):
     df = pd.read_csv(apc_csv_file_path)
     if df['graph_name'].equals(getrgfapc_df['graph_name']):
         apc_df = df
-        apc_df = apc_df.iloc[:,1:]
+        apc_df = apc_df.iloc[:,1:] #drop first column
 if os.path.exists(rapc_csv_file_path):
     df = pd.read_csv(rapc_csv_file_path)
     if df['graph_name'].equals(getrgfapc_df['graph_name']):
@@ -48,6 +54,11 @@ if os.path.exists(fcapc_csv_file_path):
         fcapc_df = df
         fcapc_df = fcapc_df.iloc[:,1:]
         fcapc_df = fcapc_df.drop('nonZeroIndex',axis = 1)
+if os.path.exists(ogapc_csv_file_path):
+    df = pd.read_csv(ogapc_csv_file_path)
+    if df['graph_name'].equals(getrgfapc_df['graph_name']):
+        ogapc_df = df
+        ogapc_df = ogapc_df.iloc[:,1:]
 if os.path.exists(nfcapc_csv_file_path):
     df = pd.read_csv(nfcapc_csv_file_path)
     if df['graph_name'].equals(getrgfapc_df['graph_name']):
@@ -57,15 +68,15 @@ if os.path.exists(nfcapc_csv_file_path):
         nfcapc_df = nfcapc_df.drop("longest time",axis = 1)
         nfcapc_df = nfcapc_df.drop("numNode",axis = 1)
 
-# print(fcapc_df)
 merge1 = pd.merge(apc_df,rapc_df)
 merge2 = pd.merge(merge1, fcapc_df)
-merge3 = pd.merge(merge2, nfcapc_df)
-merge4 = pd.merge(merge3, getrgfapc_df)
+merge3 = pd.merge(merge2, ogapc_df)
+merge4 = pd.merge(merge3, nfcapc_df)
+mergefinal = pd.merge(merge4, getrgfapc_df)
 
-new_order = ['graph_name','apc','rapc','fcapc','nfcapc','getrgfapc','apc_time','rapc_time','fcapc_time','nfcapc_time','getrgfapc_time','longest for getrgf','longest time']
-merge4 = merge4[new_order]
+new_order = ['graph_name','apc','rapc','fcapc','ogapc','nfcapc','getrgfapc','apc_time','rapc_time','fcapc_time','ogapc_time','nfcapc_time','getrgfapc_time','longest for getrgf','longest time']
+mergefinal = mergefinal[new_order]
 
-print(merge4)
+print(mergefinal)
 
-merge4.to_csv("/app/code/tests/data/merge_data.csv")
+mergefinal.to_csv("/app/code/tests/data/merge_data.csv")
