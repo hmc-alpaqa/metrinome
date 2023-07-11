c839822661bc33d92eafda9c199df3c2b1584354
YukiYang31 <yangyihan121@gmail.com>
2023-07-11 13:59:12 -0700

    changed testing timeout

--- src/benchmarkTesting.sh
+++ src/benchmarkTesting.sh
@@ -7,7 +7,3 @@ python /app/code/experiments/function_calls/summer2023_tests/test_fcapc.py
 python /app/code/experiments/function_calls/summer2023_tests/test_getrgf.py #must run this in order for mergeTestResult to work
 
 python /app/code/experiments/function_calls/summer2023_tests/mergeData.py
-
-
-
-
