10e453ce0c744ffce075b51cda420027c8ec5d5e
mkaniyur <mkaniyur@g.hmc.edu>
2023-07-14 10:19:16 -0700

    benchmark testing functions

---
+++ src/experiments/function_calls/benchmark/quicksort_recursive.c
@@ -0,0 +1,30 @@
+// sorts a list of numbers
+// apc case 2
+// from recursion/files/sorting-algorithms-quicksort-1.c
+
+#include <stdio.h>
+
+void quicksort(int *A, int len);
+
+
+
+void quicksort(int *A, int len) {
+  if (len < 2) return;
+
+  int pivot = A[len / 2];
+
+  int i, j;
+  for (i = 0, j = len - 1; ; i++, j--) {
+    while (A[i] < pivot) i++;
+    while (A[j] > pivot) j--;
+
+    if (i >= j) break;
+
+    int temp = A[i];
+    A[i]     = A[j];
+    A[j]     = temp;
+  }
+
+  quicksort(A, i);
+  quicksort(A + i, len - i);
+}
