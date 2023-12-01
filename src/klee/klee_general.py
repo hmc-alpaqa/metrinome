974352dbd7ccf2b304888ed77b3971f91958f69e
Duy Lam <tomqlam@gmail.com>
2023-11-30 23:49:43 -0800

    improved script klee

--- src/klee/klee_general.py
+++ src/klee/klee_general.py
@@ -185,68 +185,84 @@ def prepare(klee_path, src_filepath, filepath, file_generation_function, functio
         run_klee_general(name_tuple[0], filepath, klee_functions, labels, xaxis, xlabel, fields, klee_path, remove, timeout)
 
 def main() -> None:
+    def parseCFileList(filepath):
+        # Initialize an empty array to store the results
+        result = []
 
+        # Open the file and read its contents
+        with open(filepath, "r") as file:
+            # Read the file line by line
+            lines = file.readlines()
+
+            # Process each line
+            for line in lines:
+                # Strip whitespace and check if the line is not empty
+                line = line.strip()
+                if not line:
+                    continue
+
+                # Split the line into file path and function name
+                file_path, function_name = line.rsplit(' ', 1)
+
+                # Extract the directory and file name
+                dir_path, file_name_with_extension = file_path.rsplit('/', 1)
+
+                dir_path = dir_path + '/'
+
+                # Remove the .c extension from the file name
+                file_name = file_name_with_extension.replace('.c', '')
+
+                # Add the extracted information to the result array
+                result.append([dir_path, file_name, function_name])
+        return result
+    
+    functions_to_run = parseCFileList("/app/code/tests/cFiles/benchmark/kleeversions/kleetest_fall2023/TheAlgorithmsSubset.txt")
+    print(functions_to_run)
+    filepath = "/app/code/tests/cFiles/benchmark/kleeversions/kleetest_fall2023/" # directory to create (if it doesn't exist) and put all results/created files in
     klee_path = "/app/build/bin/klee" # location of the klee command
-    src_filepath = "/app/code/tests/cFiles/benchmark/kleeversions/kleetest_summer2023/cFiles/" # location of source files to run klee on, end with a slash
-    filepath = "/app/code/tests/cFiles/benchmark/kleeversions/kleetest_summer2023/" # directory to create (if it doesn't exist) and put all results/created files in
+    for function in functions_to_run:
+        print(function)
+        src_filepath = function[0] # location of source files to run klee on, end with a slash
+        klee_path = "/app/build/bin/klee"
+        klee_params_lambda = lambda output, var, filepath: f" --output-dir={output} --max-depth={var} {filepath}"
+        # klee_params_lambda = lambda output, var, filepath: f" --output-dir={output} --max-time={var}min --watchdog {filepath}"
+        # f" --output-dir={output} --max-depth={var} {filepath}"
+        # --dump-states-on-halt=false --max-time=5min
+        # lambda function to generate klee commands
+        # should take in an outpute filepath, a number to vary, and an input filepath
+        # shouldn't actually contain the call to klee
+
+        xlabel = "max depth" # label for the x-axis (parameter that is being varied)
+
+        pregenerated = []
+
+        file_generation_function = lambda src_filepath, file_path, file_name: list(zip(generate_files(src_filepath, file_path, file_name, False) if file_name not in pregenerated else generate_files_premade(src_filepath, file_path, file_name)))
+
+        functions = [function[2]]
+        # functions = ['even_odd']
+
+
+        labels = ["normal"] # the labels for the different "compilation methods"
+
+        xaxis = ['1', '2', '4', '8', '16', '32', '64', '128', '256', '512', '1024'] # max depth value
+        # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','20','25','50','100','500','1000'] # for factorial
+        #xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'] #for quicksort
+        # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','20','25','27','28','29','30','33',"35",'36'] #for fib_rec_wrapper
+        # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13','14','15','20','25','30','35','40','50','100','150'] #for binary search
+        # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','16','17'] #for partition
+        # xaxis = ['20','21','22','23','24','30','50']
+        # xaxis = ['1', '2', '3', '200']
+        timeout = 1000 #timeout
+        fields = ["ICov(%)", 'BCov(%)', "CompletedPaths", "GeneratedTests", "RealTime", "UserTime",
+                "SysTime", "PythonTime"] # all klee output fields that we are interested in
+        remove = True # whether klee files should be deleted after the important data is collected. Usually set to True
+
+        prepare(klee_path, src_filepath, filepath, file_generation_function, functions, klee_params_lambda, xlabel, labels, xaxis, fields, remove, timeout) #run the experiment
+    exit(0)
+   
+    
+    filepath = "/app/code/tests/cFiles/benchmark/kleeversions/kleetest_fall2023/" # directory to create (if it doesn't exist) and put all results/created files in
                                                                         # both filepaths needs to end with a slash
-    # src_filepath = "/app/code/tests/cFiles/fse_2020_benchmark/"
-    # filepath =   "/app/code/tests/cFiles/fse_2020_benchmark/kleetest/"
-    klee_params_lambda = lambda output, var, filepath: f" --output-dir={output} --max-depth={var} {filepath}"
-    # klee_params_lambda = lambda output, var, filepath: f" --output-dir={output} --max-time={var}min --watchdog {filepath}"
-    # f" --output-dir={output} --max-depth={var} {filepath}"
-    # --dump-states-on-halt=false --max-time=5min
-    # lambda function to generate klee commands
-    # should take in an outpute filepath, a number to vary, and an input filepath
-    # shouldn't actually contain the call to klee
-
-    xlabel = "max depth" # label for the x-axis (parameter that is being varied)
-
-
-    # pregenerated = ['lina_bac_sub', 'lina_cmp', 'lina_cofactor', 'lina_comp_transpose', 'lina_conjugate', 'lina_cramer',
-    # 'lina_create_dvector', 'lina_createvector', 'lina_crossProduct', 'lina_determinant', 'lina_disp_cmpMat', 'lina_disp_floatMat',
-    # 'lina_disp_intMat', 'lina_dotProduct', 'lina_for_sub', 'lina_has_converged', 'lina_ishermitian', 'lina_lup_sol', 'lina_matrix_inverse',
-    # 'lina_matrix_multiplication', 'lina_max', 'lina_print_dVector', 'lina_printVector', 'lina_Read_cmpMat', 'lina_Read_intMat', 'lina_Read_Vector',
-    # 'lina_rpm', 'lina_scalarTripleProduct', 'lina_scale', 'lina_transpose', 'lina_vect_mat_multiplication', 'lina_vectorTripleProduct']
-    pregenerated = []
-
-    file_generation_function = lambda src_filepath, file_path, file_name: list(zip(generate_files(src_filepath, file_path, file_name, False) if file_name not in pregenerated else generate_files_premade(src_filepath, file_path, file_name)))
-    # file_generation_function = lambda src_filepath, file_path, file_name: list(zip(generate_files(src_filepath, file_path, file_name, False), generate_files(src_filepath, file_path, file_name, True)))
-    # should be a callable object that takes in a source folder, output folder, and file name
-    # returns a tuple of names for files, if each was compiled differently (ie optimized vs normal) and also compiles/generates the necessary files for klee
-
-
-# 'lina_bac_sub', 'lina_cmp', 'lina_cofactor', 'lina_comp_transpose', 'lina_conjugate', 'lina_cramer',
-# 'lina_create_dvector', 'lina_createvector', 'lina_crossProduct', 'lina_determinant', 'lina_disp_cmpMat', 'lina_disp_floatMat',
-# 'lina_disp_intMat', 'lina_dotProduct', 'lina_for_sub', 'lina_has_converged', 'lina_ishermitian', 'lina_lup_sol', 'lina_matrix_inverse',
-# 'lina_matrix_multiplication', 'lina_max', 'lina_print_dVector', 'lina_printVector', 'lina_Read_cmpMat', 'lina_Read_intMat', 'lina_Read_Vector',
-# 'lina_rpm', 'lina_scalarTripleProduct', 'lina_scale', 'lina_transpose', 'lina_vect_mat_multiplication', 'lina_vectorTripleProduct',
-# 'recursive_func', 'merge_sort', 'quick-sort',
-    # functions = ['digital-root-multiplicative-digital-root', 'parsing-rpn-calculator-algorithm-2', 'arrays-10', 'chinese-remainder-theorem', 'multifactorial', 'combinations-1', 'spiral-matrix-2', 'non-continuous-subsequences-3', 'mutual-recursion', 'huffman-coding-2', 'ulam-spiral--for-primes--2', 'quickselect-algorithm', '24-game', 'combinations-with-repetitions', 'binary-search', 'abc-problem', 'iterated-digits-squaring-2', 'n-queens-problem-1', 'power-set', 'zebra-puzzle-1', 'percolation-site-percolation-2', 'reverse-words-in-a-string', 'greatest-common-divisor-2', 'ackermann-function-1', 'happy-numbers-1', 'catalan-numbers-1', 'permutation-test-1', 'heronian-triangles', 'factorial-3', 'factorial-5', 'factorial-4', 'towers-of-hanoi-1', 'long-multiplication-1', 'permutations-by-swapping', 'balanced-ternary', 'call-an-object-method', 'truncatable-primes-3', 'pascals-triangle-2', 'permutations-derangements', 'matrix-arithmetic-1', 'topswops-1', 'sorting-algorithms-stooge-sort', 'hailstone-sequence-2', 'sorting-algorithms-radix-sort', 'stern-brocot-sequence-1', 'visualize-a-tree', 'dinesmans-multiple-dwelling-problem', 'sorting-algorithms-quicksort-1', 'sorting-algorithms-quicksort-2', 'sierpinski-carpet-3', 'find-limit-of-recursion-2', 'find-limit-of-recursion-1', 'palindrome-detection-3', 'ordered-partitions-2', 'sudoku', 'fibonacci-sequence-1', 'levenshtein-distance-1']
-    # functions = ['lina_determinant']
-    functions = ['partition']
-    # functions = ['even_odd']
-
-
-    labels = ["normal"] # the labels for the different "compilation methods"
-
-    # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','20','25','50','100','500','1000'] # for even_odd
-    # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','20','25','50','100','500','1000'] # for factorial
-    #xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'] #for quicksort
-    # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','20','25','27','28','29','30','33',"35",'36'] #for fib_rec_wrapper
-    # xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13','14','15','20','25','30','35','40','50','100','150'] #for binary search
-    xaxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15','16','17'] #for partition
-    # xaxis = ['20','21','22','23','24','30','50']
-    # xaxis = ['1', '2', '3', '200']
-    timeout = 1000 #timeout
-    fields = ["ICov(%)", 'BCov(%)', "CompletedPaths", "GeneratedTests", "RealTime", "UserTime",
-              "SysTime", "PythonTime"] # all klee output fields that we are interested in
-    remove = True # whether klee files should be deleted after the important data is collected. Usually set to True
-
-    prepare(klee_path, src_filepath, filepath, file_generation_function, functions, klee_params_lambda, xlabel, labels, xaxis, fields, remove, timeout) #run the experiment
-
-
-
 
 if __name__ == "__main__":
     main()
