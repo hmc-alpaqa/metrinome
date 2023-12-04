9314031f6c601e4623405dd517446172bdd121c1
YukiYang31 <yangyihan121@gmail.com>
2023-12-03 16:57:07 -0800

    [Tom] Commit stuff in case we need it

--- src/Testing.sh
+++ src/Testing.sh
@@ -1,6 +1,6 @@
-python /app/code/experiments/function_calls/summer2023_tests/test_rapc.py
+# python /app/code/experiments/function_calls/summer2023_tests/test_rapc.py
 
-python /app/code/experiments/function_calls/summer2023_tests/test_fcapc.py
+# python /app/code/experiments/function_calls/summer2023_tests/test_fcapc.py
 
 python3 /app/code/experiments/function_calls/summer2023_tests/test_getrgf.py
 # #must run this in order for mergeTestResult to work
