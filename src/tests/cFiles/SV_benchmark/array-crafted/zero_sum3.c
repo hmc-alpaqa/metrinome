ae38569f51399bd241b668cddf791aa58a8917d5
YukiYang31 <yangyihan121@gmail.com>
2024-02-04 16:09:38 -0800

    add SV benchmark first folder in for sample testing

---
+++ src/tests/cFiles/SV_benchmark/array-crafted/zero_sum3.c
@@ -0,0 +1,55 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+extern short __VERIFIER_nondet_short(void);
+
+short SIZE;
+
+int main()
+{
+	SIZE = __VERIFIER_nondet_short();
+	if(SIZE > 1)
+	{
+		int i;
+		short a[SIZE];
+		long long sum=0;
+		
+		for(int i = 0; i < SIZE; i++) 
+		{
+		  a[i] = __VERIFIER_nondet_short();
+		}
+
+		for(i = 0; i < SIZE; i++ )
+		{
+			sum = sum + a[i];
+		}
+
+		for(i = 0; i < SIZE; i++)
+		{
+			sum = sum - a[i];
+		}
+
+		for(i = 0; i < SIZE; i++ )
+		{
+			sum = sum + a[i];
+		}
+
+		for(i = 0; i < SIZE; i++)
+		{
+			sum = sum - a[i];
+		}
+
+		for(i = 0; i < SIZE; i++ )
+                {
+                        sum = sum + a[i];
+                }
+
+                for(i = 0; i < SIZE; i++)
+                {
+                        sum = sum - a[i];
+                }
+
+		__VERIFIER_assert(sum == 0);
+	}
+	return 1;
+}
---
+++ src/tests/cFiles/SV_benchmark/array-crafted/zero_sum_const_m3.c
@@ -0,0 +1,57 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern void __VERIFIER_assume(int);
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: __VERIFIER_error(); } }
+extern long __VERIFIER_nondet_long(void);
+void *malloc(unsigned int size);
+
+long SIZE;
+
+const int MAX = 100000;
+
+int main()
+{
+	SIZE = __VERIFIER_nondet_long();
+	if(SIZE > 1 && SIZE < MAX)
+	{
+		int i;
+		long *a = malloc(sizeof(long)*SIZE);
+		long long sum=0;
+
+		for(i = 0; i < SIZE; i++ )
+		{
+			a[i] = 1;
+		}
+
+		for(i = 0; i < SIZE; i++ )
+		{
+			sum = sum + a[i];
+		}
+
+		for(i = 0; i < SIZE; i++ )
+		{
+			sum = sum + a[i];
+		}
+
+		for(i = 0; i < SIZE; i++ )
+		{
+			sum = sum + a[i];
+		}
+
+		for(i = 0; i < SIZE; i++)
+		{
+			sum = sum - a[i];
+		}
+
+		for(i = 0; i < SIZE; i++)
+		{
+			sum = sum - a[i];
+		}
+
+		for(i = 0; i < SIZE; i++)
+		{
+			sum = sum - a[i];
+		}
+		__VERIFIER_assert(sum == 0);
+	}
+	return 1;
+}
