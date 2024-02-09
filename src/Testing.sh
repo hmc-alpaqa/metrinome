a345217e1772fd489b39ec0e5b3b50e0d7b38dca
mkaniyur <mkaniyur@g.hmc.edu>
2024-02-08 16:47:34 -0800

    update for docker

--- src/Testing.sh
+++ src/Testing.sh
@@ -5,4 +5,4 @@
 python3 /app/code/experiments/function_calls/summer2023_tests/test_getrgf.py
 # #must run this in order for mergeTestResult to work
 
-python /app/code/experiments/function_calls/summer2023_tests/mergeData.py
+python3 /app/code/experiments/function_calls/summer2023_tests/mergeData.py
